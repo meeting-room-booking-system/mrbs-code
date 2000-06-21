23c0ebb9f97838cf3720739a59ab07f15f427ca3
Daniel <nambit@users.sourceforge.net>
2000-06-21 13:53:35 +0000

    LDAP Auth

---
+++ auth_ldap.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+
+$server = shift;
+$dn = shift;
+$password = shift;
+
+use Net::LDAP qw(LDAP_SUCCESS);
+
+$ldap = Net::LDAP->new($server) or exit 1;
+
+$msg = $ldap->bind(dn => $dn, password => $password);
+
+exit $msg->code;
