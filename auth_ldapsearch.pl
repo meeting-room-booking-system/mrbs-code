4217df9753c12318c1a4dfd5b899298ceb8300df
Sam Mason <sam_mason@users.sourceforge.net>
2000-08-10 12:45:09 +0000

    better ldapsearch support

--- auth_ldapsearch.pl
+++ auth_ldapsearch.pl
@@ -3,8 +3,22 @@ my $server = shift;
 my $searchroot  = shift;
 my $uid = shift;
 my $password = shift;
-$uid = lc($uid);
-$uid =~ s/ /_/g;
+
+# If you want all login names to be converted to lower case
+# and no spaces, uncomment the next 2 lines.  Personally,
+# I don't think that's wise.
+#$uid = lc($uid);
+#$uid =~ s/ /_/g;
+
+# This line enforces that login names are entered in
+# lower case with no spaces.  This insures that the
+# create_by entries will be consistent.
+# Otherwise, create_by entries could contain
+# "Mark Belanger", "mark_belanger", and "Mark_Belanger" which
+# would cause problems with a user being able to alter/delete
+# their own meetings.
+if(($uid =~ m/[A-Z]/) || ($uid =~ /\s/)){ exit 1;}
+
 my $ldapsearch=qq!/usr/bin/ldapsearch -h $server  -b "$searchroot" "(uid=$uid)"  dn!;
 $dn=`$ldapsearch`;
 chomp $dn;
