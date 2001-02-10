73caba0be7e7822981eee258a613ea6dccfebf93
L J Bayuk <lbayuk@users.sourceforge.net>
2001-02-10 22:42:51 +0000

    A few sample buildings and rooms for testing. (This used to be in the table creation script.)

---
+++ sample-data.sql
@@ -0,0 +1,19 @@
+-- Sample data - make some areas and rooms. (This used to be in tables.sql)
+-- This is meant to go into an empty database only!
+
+-- Generate some default areas
+INSERT INTO mrbs_area ( area_name ) VALUES ('Building 1');
+INSERT INTO mrbs_area ( area_name ) VALUES ('Building 2');
+
+-- Generate some default rooms
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 1, 'Room 1', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 1, 'Room 2', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 1, 'Room 3', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 1, 'Room 4', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 2, 'Room 1', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 2, 'Room 2', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 2, 'Room 3', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 2, 'Room 4', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 2, 'Room 5', '', 8);
+INSERT INTO mrbs_room ( area_id, room_name, description, capacity ) VALUES ( 2, 'Room 6', '', 8);
+
