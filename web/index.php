f1988488a19b03225f9f6a2ee277ce2f84b19062
L J Bayuk <lbayuk@users.sourceforge.net>
2001-02-10 22:34:16 +0000

    Account for files renamed from *.php3 to *.php.

---
+++ web/index.php
@@ -0,0 +1,9 @@
+<?
+#Index is just a stub to redirect to the appropriate day view
+
+$day   = date("d");
+$month = date("m");
+$year  = date("Y");
+
+header("Location: day.php?day=$day&month=$month&year=$year");
+
