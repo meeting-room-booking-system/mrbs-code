74b271ac16803554b3485eac67c2cfa23182f481
L J Bayuk <lbayuk@users.sourceforge.net>
2001-02-10 22:40:30 +0000

    Renamed to *.php from *.php3 per recommendations for PHP4. Changes to support new database API for MySQL and PostgreSQL support. SQL change, reword left join as predicate-based join. Don't assume area 1 exists, and don't force area to 0. Better SQL error handling; always present full HTML pages on errors. Don't quote numeric values in SQL. Different but equivalent fix as 0.9pre2 for I18 time units and weekday names. Enhancements to support up to 10 site-configured meeting types. Enhancement to 0.0pre2 fix so week can start on any day. HTML fixups: quote tag attributes where needed by HTML standard.
    Don't use stripslashes on database records; the extra backslashes this was
    meant to remove should not have been in there in the first place.
    
    Remove code which tried to intelligently report start and end date and
    time based on duration.  Sometimes it omitted the end date when it should
    have been there, for example a 4 hour meeting which crossed midnight.  Now,
    always report both date and time for both start and end.

---
+++ web/view_entry.php
@@ -0,0 +1,172 @@
+<?php
+
+include "config.inc";
+include "functions.inc";
+include "$dbsys.inc";
+
+#If we dont know the right date then make it up
+if(!isset($day) or !isset($month) or !isset($year))
+{
+	$day   = date("d");
+	$month = date("m");
+	$year  = date("Y");
+}
+if(empty($area))
+	$area = get_default_area();
+
+print_header($day, $month, $year, $area);
+
+$sql = "
+SELECT mrbs_entry.name,
+       mrbs_entry.description,
+       mrbs_entry.create_by,
+       mrbs_room.room_name,
+       mrbs_area.area_name,
+       mrbs_entry.type,
+       mrbs_entry.room_id,
+       mrbs_entry.repeat_id,
+    " . sql_syntax_timestamp_to_unix("mrbs_entry.timestamp") . ",
+       (mrbs_entry.end_time - mrbs_entry.start_time),
+       mrbs_entry.start_time,
+       mrbs_entry.end_time
+
+FROM mrbs_entry, mrbs_room, mrbs_area
+WHERE mrbs_entry.room_id = mrbs_room.id
+  AND mrbs_room.area_id = mrbs_area.id
+  AND mrbs_entry.id=$id
+";
+
+$res = sql_query($sql);
+if (! $res) fatal_error(0, sql_error());
+
+if(sql_count($res) < 1) fatal_error(0, "Invalid entry id.");
+
+$row = sql_row($res, 0);
+sql_free($res);
+
+# Note: Removed stripslashes() calls from name and description. Previous
+# versions of MRBS mistakenly had the backslash-escapes in the actual database
+# records because of an extra addslashes going on. Fix your database and
+# leave this code alone, please.
+$name         = htmlspecialchars($row[0]);
+$description  = htmlspecialchars($row[1]);
+$create_by    = htmlspecialchars($row[2]);
+$room_name    = htmlspecialchars($row[3]);
+$area_name    = htmlspecialchars($row[4]);
+$type         = $row[5];
+$room_id      = $row[6];
+$repeat_id    = $row[7];
+$updated      = strftime('%X - %A %d %B %Y', $row[8]);
+$duration     = $row[9];
+
+$start_date = strftime('%X - %A %d %B %Y', $row[10]);
+$end_date = strftime('%X - %A %d %B %Y', $row[11]);
+
+$rep_type = 0;
+
+if($repeat_id != 0)
+{
+	$res = sql_query("SELECT rep_type, end_date, rep_opt
+	                    FROM mrbs_repeat WHERE id=$repeat_id");
+	if (! $res) fatal_error(0, sql_error());
+
+	if (sql_count($res) == 1)
+	{
+		$row = sql_row($res, 0);
+		
+		$rep_type     = $row[0];
+		$rep_end_date = strftime('%A %d %B %Y',$row[1]);
+		$rep_opt      = $row[2];
+	}
+	sql_free($res);
+}
+
+toTimeString($duration, $dur_units);
+
+$repeat_key = "rep_type_" . $rep_type;
+
+# Now that we know all the data we start drawing it
+
+?>
+
+<H3><? echo $name ?></H3>
+ <table border=0>
+   <tr>
+    <td><b><? echo $lang["description"] ?></b></td>
+    <td><?    echo nl2br($description)  ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["room"]                           ?></b></td>
+    <td><?    echo  nl2br($area_name . " - " . $room_name) ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["start_date"] ?></b></td>
+    <td><?    echo $start_date         ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["duration"]            ?></b></td>
+    <td><?    echo $duration . " " . $dur_units ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["end_date"] ?></b></td>
+    <td><?    echo $end_date         ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["type"]   ?></b></td>
+    <td><?    echo empty($typel[$type]) ? "?$type?" : $typel[$type]  ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["createdby"] ?></b></td>
+    <td><?    echo $create_by         ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["lastupdate"] ?></b></td>
+    <td><?    echo $updated            ?></td>
+   </tr>
+   <tr>
+    <td><b><? echo $lang["rep_type"]  ?></b></td>
+    <td><?    echo $lang[$repeat_key] ?></td>
+   </tr>
+<?
+
+if($rep_type != 0)
+{
+	$opt = "";
+	if ($rep_type == 2)
+	{
+		# Display day names according to language and preferred weekday start.
+		for ($i = 0; $i < 7; $i++)
+		{
+			$daynum = ($i + $weekstarts) % 7;
+			if ($rep_opt[$daynum]) $opt .= day_name($daynum) . " ";
+		}
+	}
+	
+	if($opt)
+		echo "<tr><td><b>$lang[rep_rep_day]</b></td><td>$opt</td></tr>\n";
+	
+	echo "<tr><td><b>$lang[rep_end_date]</b></td><td>$rep_end_date</td></tr>\n";
+}
+
+?>
+</table>
+<br>
+<p>
+<a href="edit_entry.php?id=<? echo $id ?>"><? echo $lang["editentry"] ?></a>
+<?
+
+if($repeat_id)
+	echo " - <a href=\"edit_entry.php?id=$id&edit_type=series&day=$day&month=$month&year=$year\">$lang[editseries]</a>";
+
+?>
+<BR>
+<A HREF="del_entry.php?id=<? echo $id ?>&series=0" onClick="return confirm('<? echo $lang["confirmdel"] ?>');"><? echo $lang["deleteentry"] ?></A>
+<?
+
+if($repeat_id)
+	echo " - <A HREF=\"del_entry.php?id=$id&series=1&day=$day&month=$month&year=$year\" onClick=\"return confirm('$lang[confirmdel]');\">$lang[deleteseries]</A>";
+
+?>
+<BR>
+<a href="<? echo $HTTP_REFERER ?>"><? echo $lang["returnprev"] ?></a>
+<? include "trailer.inc"; ?>
