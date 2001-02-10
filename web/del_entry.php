2fb82347bdb1bf82d4ed0801f27b2e7c11cd0998
L J Bayuk <lbayuk@users.sourceforge.net>
2001-02-10 22:31:55 +0000

    Renamed to *.php from *.php3 per recommendations for PHP4. Call mrbsDelEntry inside an SQL transaction (if the database supports it) to improve PostgreSQL performance when deleting a series.

---
+++ web/del_entry.php
@@ -0,0 +1,28 @@
+<?php
+
+include "config.inc";
+include "functions.inc";
+include "$dbsys.inc";
+include "mrbs_auth.inc";
+include "mrbs_sql.inc";
+
+if(getAuthorised(getUserName(), getUserPassword(), 1) && ($info = mrbsGetEntryInfo($id)))
+{
+	$day   = strftime("%d", $info["start_time"]);
+	$month = strftime("%m", $info["start_time"]);
+	$year  = strftime("%Y", $info["start_time"]);
+	$area  = mrbsGetRoomArea($info["room_id"]);
+	
+	sql_begin();
+	$result = mrbsDelEntry(getUserName(), $id, $series, 1);
+	sql_commit();
+	if ($result)
+	{
+		Header("Location: day.php?day=$day&month=$month&year=$year&area=$area");
+		exit();
+	}
+}
+
+// If you got this far then we got an access denied.
+showAccessDenied($day, $month, $year, $area);
+?>
