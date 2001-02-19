dc3c8fd6ebfaf58d2ee6ad5e1babeee2bd24d2bb
L J Bayuk <lbayuk@users.sourceforge.net>
2001-02-19 02:55:03 +0000

    Add PHP version display.

--- web/help.php
+++ web/help.php
@@ -6,7 +6,7 @@ include "config.inc";
 include "$dbsys.inc";
 include "functions.inc";
 
-$mrbs_version = "MRBS 1.0-pre1";
+$mrbs_version = "MRBS 1.0-pre2";
 
 #If we dont know the right date then make it up
 if(!isset($day) or !isset($month) or !isset($year))
@@ -21,11 +21,11 @@ if(empty($area))
 print_header($day, $month, $year, $area);
 
 echo "<H3>About MRBS</H3>\n";
-$uname = posix_uname();
-$dbms = sql_version();
 echo "<P><a href=\"http://mrbs.sourceforge.net\">$lang[mrbs]</a> - $mrbs_version\n";
-echo "<P>Database: $dbms\n";
-echo "<P>System: $uname[sysname] $uname[release] $uname[machine]\n";
+echo "<BR>Database: " . sql_version() . "\n";
+$uname = posix_uname();
+echo "<BR>System: $uname[sysname] $uname[release] $uname[machine]\n";
+echo "<BR>PHP: " . phpversion() . "\n";
 
 echo "<H3>Help</H3>\n";
 echo 'Please contact <a href="mailto:' . $mrbs_admin_email
