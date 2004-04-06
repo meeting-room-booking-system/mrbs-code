a7a42c52e098357760bb03d3e5919982e4bdaeea
Thierry Bothorel <thierry_bo@users.sourceforge.net>
2004-04-06 02:27:07 +0000

    Sync with MRBS HEAD. Added configuration option to allow index.php to redirect to a specific room.

--- web/index.php
+++ web/index.php
@@ -2,9 +2,15 @@
 
 # $Id$
 
-#Index is just a stub to redirect to the appropriate day view
+# Index is just a stub to redirect to the appropriate view
+# as defined in config.inc.php using the variable $default_view
+# If $default_room is defined in config.inc.php then this will
+# be used to redirect to a particular room.
+
 require_once "grab_globals.inc.php";
 include("config.inc.php");
+require_once("database.inc.php");
+require "$dbsys.inc";
 
 $day   = date("d");
 $month = date("m");
@@ -13,12 +19,27 @@ $year  = date("Y");
 switch ($default_view)
 {
 	case "month":
-		header("Location: month.php?year=$year&month=$month");
+		$redirect_str = "month.php?year=$year&month=$month";
 		break;
 	case "week":
-		header("Location: week.php?year=$year&month=$month&day=$day");
+		$redirect_str = "week.php?year=$year&month=$month&day=$day";
 		break;
 	default:
-		header("Location: day.php?day=$day&month=$month&year=$year");
+        $redirect_str = "day.php?day=$day&month=$month&year=$year";
 }
+
+if( ! empty($default_room) )
+{
+
+    $sql = "SELECT area_id FROM mrbs_room WHERE id=$default_room";
+    $area = $mdb->queryOne($sql);
+    if (!MDB::isError($area))
+    {
+        $room = $default_room;
+        $redirect_str .= "&area=$area&room=$room";
+    }
+}
+
+header("Location: $redirect_str");
+
 ?>
