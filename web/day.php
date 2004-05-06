13ccf14ac8715ff068937261ccac69438389a60a
Gavin Walker <gwalker@users.sourceforge.net>
2004-05-06 21:58:01 +0000

    Modified so that capacity is not displayed in brackets next to room name if capacity is 0.  This helps where the "room" is a piece of equipment or a person.  Thanks to Michael Hellwig.

--- web/day.php
+++ web/day.php
@@ -222,7 +222,7 @@ else
         echo "<th width=\"$room_column_width%\">
             <a href=\"week.php?year=$year&month=$month&day=$day&area=$area&room=$row[2]\"
             title=\"" . get_vocab("viewweek") . " &#10;&#10;$row[3]\">"
-            . htmlspecialchars($row[0]) . "($row[1])</a></th>";
+            . htmlspecialchars($row[0]) . ($row[1] > 0 ? "($row[1])" : "") . "</a></th>";
 		$rooms[] = $row[2];
 	}
 
