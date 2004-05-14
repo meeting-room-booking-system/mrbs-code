a475dcd03040299292a4a40e07a035d71e638b64
Thierry Bothorel <thierry_bo@users.sourceforge.net>
2004-05-14 02:35:02 +0000

    Dox fix: This script can be used with both mysql and postgresql

--- upgrade3.my.sql
+++ upgrade3.my.sql
@@ -1,6 +1,10 @@
 # $Id$
 
-# Add an extra column to the mrbs_area and mrbs_romm table for emails handling
+# Run this script to upgrade any postgresql or mysqlmrbs version prior to
+# 1.2-pre3.
+
+# Add an extra column to the mrbs_area and mrbs_room table for emails handling
+
 ALTER TABLE mrbs_area
 ADD COLUMN area_admin_email text;
 ALTER TABLE mrbs_room
