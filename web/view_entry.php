717c4f01a1aff7b422b7a0be6c9eb64efae2f643
Gavin Walker <gwalker@users.sourceforge.net>
2004-06-23 21:06:52 +0000

    Changed view_entry.php so that it could handle the situation where an id for a series is passed to it.  A series is identified by series=1 in the url.
    Changed functions_mail.inc to generate urls with series=1 in the email
    messages where the message relates to a series.
    
    Changed lang.en to have an error message for when an invalid series id is
    passed to view_entry.php.
    
    Fixes bug 972194
    
    
    Fixed bug

--- web/view_entry.php
+++ web/view_entry.php
@@ -18,30 +18,68 @@ if(empty($area))
 
 print_header($day, $month, $year, $area);
 
-$sql = "
-SELECT $tbl_entry.name,
-       $tbl_entry.description,
-       $tbl_entry.create_by,
-       $tbl_room.room_name,
-       $tbl_area.area_name,
-       $tbl_entry.type,
-       $tbl_entry.room_id,
-       $tbl_entry.repeat_id,
-    " . sql_syntax_timestamp_to_unix("$tbl_entry.timestamp") . ",
-       ($tbl_entry.end_time - $tbl_entry.start_time),
-       $tbl_entry.start_time,
-       $tbl_entry.end_time
+if( empty($series) ) {
+	$series = 0;
+}
+else {
+	$series = 1;
+}
 
-FROM $tbl_entry, $tbl_room, $tbl_area
-WHERE $tbl_entry.room_id = $tbl_room.id
-  AND $tbl_room.area_id = $tbl_area.id
-  AND $tbl_entry.id=$id
-";
+if( $series ){
+	$sql = "
+	SELECT $tbl_repeat.name,
+	       $tbl_repeat.description,
+	       $tbl_repeat.create_by,
+	       $tbl_room.room_name,
+	       $tbl_area.area_name,
+	       $tbl_repeat.type,
+	       $tbl_repeat.room_id,
+	       " . sql_syntax_timestamp_to_unix("$tbl_repeat.timestamp") . ",
+	       ($tbl_repeat.end_time - $tbl_repeat.start_time),
+	       $tbl_repeat.start_time,
+	       $tbl_repeat.end_time,
+	       $tbl_repeat.rep_type,
+	       $tbl_repeat.end_date,
+	       $tbl_repeat.rep_opt,
+	       $tbl_repeat.rep_num_weeks
+
+	FROM  $tbl_repeat, $tbl_room, $tbl_area
+	WHERE $tbl_repeat.room_id = $tbl_room.id
+		AND $tbl_room.area_id = $tbl_area.id
+		AND $tbl_repeat.id=$id
+	";
+}
+else {
+	$sql = "
+	SELECT $tbl_entry.name,
+	       $tbl_entry.description,
+	       $tbl_entry.create_by,
+	       $tbl_room.room_name,
+	       $tbl_area.area_name,
+	       $tbl_entry.type,
+	       $tbl_entry.room_id,
+	       " . sql_syntax_timestamp_to_unix("$tbl_entry.timestamp") . ",
+	       ($tbl_entry.end_time - $tbl_entry.start_time),
+	       $tbl_entry.start_time,
+	       $tbl_entry.end_time,
+	       $tbl_entry.repeat_id
+
+	FROM  $tbl_entry, $tbl_room, $tbl_area
+	WHERE $tbl_entry.room_id = $tbl_room.id
+		AND $tbl_room.area_id = $tbl_area.id
+		AND $tbl_entry.id=$id
+	";
+}
 
 $res = sql_query($sql);
 if (! $res) fatal_error(0, sql_error());
 
-if(sql_count($res) < 1) fatal_error(0, get_vocab("invalid_entry_id"));
+if(sql_count($res) < 1) {
+	fatal_error(
+		0,
+		($series ? get_vocab("invalid_series_id") : get_vocab("invalid_entry_id"))
+	);
+}
 
 $row = sql_row($res, 0);
 sql_free($res);
@@ -57,42 +95,85 @@ $room_name    = htmlspecialchars($row[3]);
 $area_name    = htmlspecialchars($row[4]);
 $type         = $row[5];
 $room_id      = $row[6];
-$repeat_id    = $row[7];
-$updated      = time_date_string($row[8]);
+$updated      = time_date_string($row[7]);
 # need to make DST correct in opposite direction to entry creation
 # so that user see what he expects to see
-$duration     = $row[9] - cross_dst($row[10], $row[11]);
+$duration     = $row[8] - cross_dst($row[9], $row[10]);
 
 if( $enable_periods )
-	list( $start_period, $start_date) =  period_date_string($row[10]);
+	list( $start_period, $start_date) =  period_date_string($row[9]);
 else
-        $start_date = time_date_string($row[10]);
+        $start_date = time_date_string($row[9]);
 
 if( $enable_periods )
-	list( , $end_date) =  period_date_string($row[11], -1);
+	list( , $end_date) =  period_date_string($row[10], -1);
 else
-        $end_date = time_date_string($row[11]);
+        $end_date = time_date_string($row[10]);
 
 
 $rep_type = 0;
 
-if($repeat_id != 0)
-{
-	$res = sql_query("SELECT rep_type, end_date, rep_opt, rep_num_weeks
-	                    FROM $tbl_repeat WHERE id=$repeat_id");
-	if (! $res) fatal_error(0, sql_error());
+if( $series == 1 ){
 
-	if (sql_count($res) == 1)
-	{
-		$row = sql_row($res, 0);
-		
-		$rep_type     = $row[0];
-		$rep_end_date = utf8_strftime('%A %d %B %Y',$row[1]);
-		$rep_opt      = $row[2];
-		$rep_num_weeks = $row[3];
+	$rep_type     = $row[11];
+	$rep_end_date = utf8_strftime('%A %d %B %Y',$row[12]);
+	$rep_opt      = $row[13];
+	$rep_num_weeks = $row[14];
+	# I also need to set $id to the value of a single entry as it is a
+	# single entry from a series that is used by del_entry.php and
+	# edit_entry.php
+	# So I will look for the first entry in the series where the entry is
+	# as per the original series settings
+	$sql = "SELECT id
+	        FROM $tbl_entry
+		WHERE repeat_id=\"$id\" AND entry_type=\"1\"
+		ORDER BY start_time
+		LIMIT 1";
+	$res = sql_query($sql);
+	if (! $res) fatal_error(0, sql_error());
+	if(sql_count($res) < 1) {
+		# if all entries in series have been modified then
+		# as a fallback position just select the first entry
+		# in the series
+		# hopefully this code will never be reached as
+		# this page will display the start time of the series
+		# but edit_entry.php will display the start time of the entry
+		sql_free($res);
+		$sql = "SELECT id
+			FROM $tbl_entry
+			WHERE repeat_id=\"$id\"
+			ORDER BY start_time
+			LIMIT 1";
+		$res = sql_query($sql);
+		if (! $res) fatal_error(0, sql_error());
 	}
+	$row = sql_row($res, 0);
+	$id = $row[0];
 	sql_free($res);
 }
+else {
+
+	$repeat_id = $row[11];
+
+	if($repeat_id != 0)
+	{
+		$res = sql_query("SELECT rep_type, end_date, rep_opt, rep_num_weeks
+				FROM $tbl_repeat WHERE id=$repeat_id");
+		if (! $res) fatal_error(0, sql_error());
+
+		if (sql_count($res) == 1)
+		{
+			$row = sql_row($res, 0);
+
+			$rep_type     = $row[0];
+			$rep_end_date = utf8_strftime('%A %d %B %Y',$row[1]);
+			$rep_opt      = $row[2];
+			$rep_num_weeks = $row[3];
+		}
+		sql_free($res);
+	}
+}
+
 
 $enable_periods ? toPeriodString($start_period, $duration, $dur_units) : toTimeString($duration, $dur_units);
 
@@ -169,19 +250,27 @@ if($rep_type != 0)
 </table>
 <br>
 <p>
-<a href="edit_entry.php?id=<?php echo $id ?>"><?php echo get_vocab("editentry") ?></a>
 <?php
+if( ! $series )
+	echo "<a href=\"edit_entry.php?id=$id\">". get_vocab("editentry") ."</a>";
 
 if($repeat_id)
-	echo " - <a href=\"edit_entry.php?id=$id&edit_type=series&day=$day&month=$month&year=$year\">".get_vocab("editseries")."</a>";
+	echo " - ";
+
+if($repeat_id || $series )
+	echo "<a href=\"edit_entry.php?id=$id&edit_type=series&day=$day&month=$month&year=$year\">".get_vocab("editseries")."</a>";
 
 ?>
 <BR>
-<A HREF="del_entry.php?id=<?php echo $id ?>&series=0" onClick="return confirm('<?php echo get_vocab("confirmdel") ?>');"><?php echo get_vocab("deleteentry") ?></A>
 <?php
+if( ! $series )
+	echo "<A HREF=\"del_entry.php?id=$id&series=0\" onClick=\"return confirm('".get_vocab("confirmdel")."');\">".get_vocab("deleteentry")."</A>";
 
 if($repeat_id)
-	echo " - <A HREF=\"del_entry.php?id=$id&series=1&day=$day&month=$month&year=$year\" onClick=\"return confirm('".get_vocab("confirmdel")."');\">".get_vocab("deleteseries")."</A>";
+	echo " - ";
+
+if($repeat_id || $series )
+	echo "<A HREF=\"del_entry.php?id=$id&series=1&day=$day&month=$month&year=$year\" onClick=\"return confirm('".get_vocab("confirmdel")."');\">".get_vocab("deleteseries")."</A>";
 
 ?>
 <BR>
