4c6a7da1d3aae81cdc626749af287eb00d3e0a62
John Beranek <jberanek@users.sourceforge.net>
2008-03-21 21:24:24 +0000

    * Upped width of mrbs_entry.create_by to 80 characters, to match the  other schemas.

--- tables.pg.73and_above.sql
+++ tables.pg.73and_above.sql
@@ -51,7 +51,7 @@ CREATE TABLE mrbs_entry
   repeat_id   int DEFAULT 0 NOT NULL,
   room_id     int DEFAULT 1 NOT NULL,
   timestamp   timestamp DEFAULT current_timestamp,
-  create_by   varchar(25) NOT NULL,
+  create_by   varchar(80) NOT NULL,
   name        varchar(80) NOT NULL,
   type        char DEFAULT 'E' NOT NULL,
   description text
