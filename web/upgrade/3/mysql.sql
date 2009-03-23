e92a26ec351774f23af8195fa0aaf7cbd55b24a5
dceschools <devnull@localhost>
2009-03-23 20:13:13 +0000

    Corrected DB upgrade for private bookings to use configured DB prefix instead of default mrbs_ prefix.

--- web/upgrade/3/mysql.sql
+++ web/upgrade/3/mysql.sql
@@ -3,7 +3,7 @@
 -- Add an extra column to the mrbs_entry and mrbs_repeat table 
 -- for private bookings handling
 
-ALTER TABLE mrbs_repeat 
+ALTER TABLE %DB_TBL_PREFIX%repeat 
  ADD private BOOL NOT NULL DEFAULT '0';
-ALTER TABLE mrbs_entry 
+ALTER TABLE %DB_TBL_PREFIX%entry 
  ADD private BOOL NOT NULL DEFAULT '0';
