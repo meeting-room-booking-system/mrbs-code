fb9423c81b383d0f47a7184a1f01fc95f20e475e
Campbell Morrison <cimorrison@users.sourceforge.net>
2009-11-09 21:27:22 +0000

    Updated the db version number.

--- tables.pg.sql
+++ tables.pg.sql
@@ -99,6 +99,6 @@ CREATE TABLE mrbs_users
 );
 
 INSERT INTO mrbs_variables (variable_name, variable_content)
-  VALUES ('db_version', '7');
+  VALUES ('db_version', '8');
 INSERT INTO mrbs_variables (variable_name, variable_content)
   VALUES ('local_db_version', '1');
