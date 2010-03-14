500324eb8192c00c9455eb0b57c6618c0c79535c
Campbell Morrison <cimorrison@users.sourceforge.net>
2010-03-14 21:37:43 +0000

    - Removed the Submit button from Area and Room select forms when JavaScript is enabled as they are redundant. - Minor tidy up of JavaScript

--- web/mrbs-js-overrides.css.php
+++ web/mrbs-js-overrides.css.php
@@ -17,3 +17,11 @@ div.multiple_control {
     display: block;   /* if JavaScript is enabled then we want to see the JavaScript controls */
   }
 .multiple_booking .maxi a {padding-left: <?php echo $main_cell_height + $main_table_cell_border_width + 2 ?>px}
+
+<?php
+// Don't display anything with a class of js_none (used for example for hiding Submit
+// buttons when we're submitting onchange)
+?>
+.js_none {
+    display: none;
+  }
