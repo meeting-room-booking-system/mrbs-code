37ea0c44cd7a8dbf616b9718701084a4b4d3a19b
John Beranek <jberanek@users.sourceforge.net>
2010-09-22 12:54:28 +0000

    * Made banner date field a little wider, it didn't fit "22/09/2010" for  me.

--- web/mrbs.css.php
+++ web/mrbs.css.php
@@ -667,7 +667,7 @@ table#banner {width: 100%; border-spacing: 0; border-collapse: collapse;
 #banner a:link    {color: <?php echo $anchor_link_color_banner ?>;    text-decoration: none; font-weight: normal}
 #banner a:visited {color: <?php echo $anchor_visited_color_banner ?>; text-decoration: none; font-weight: normal}
 #banner a:hover   {color: <?php echo $anchor_hover_color_banner ?>;   text-decoration:underline; font-weight: normal}
-#banner input.date {width: 6em}
+#banner input.date {width: 6.5em}
 
 table#colour_key {clear: both; border-spacing: 0; border-collapse: collapse}
 #colour_key td {width: 7.0em; padding: 2px; font-weight: bold;
