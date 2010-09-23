9d03bf3e1a079105d9c7c0cb292479d5f8b92ba8
Campbell Morrison <cimorrison@users.sourceforge.net>
2010-09-23 15:46:19 +0000

    Set the default setting to 'pt' instead of 'pt-BR'

--- web/jquery/ui/jquery-ui-datepicker-pt.js
+++ web/jquery/ui/jquery-ui-datepicker-pt.js
@@ -20,5 +20,5 @@ jQuery(function($){
 		isRTL: false,
 		showMonthAfterYear: false,
 		yearSuffix: ''};
-	$.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+	$.datepicker.setDefaults($.datepicker.regional['pt']);
 });﻿
