4d59b7d81f2f60011c50bdbb7a49ffdb832310a2
Campbell Morrison <cimorrison@users.sourceforge.net>
2010-12-21 12:04:46 +0000

    Tidied up allocation of sequence numbers to existing bookings

--- tables.pg.sql
+++ tables.pg.sql
@@ -142,6 +142,6 @@ CREATE TABLE mrbs_users
 );
 
 INSERT INTO mrbs_variables (variable_name, variable_content)
-  VALUES ('db_version', '24');
+  VALUES ('db_version', '25');
 INSERT INTO mrbs_variables (variable_name, variable_content)
   VALUES ('local_db_version', '1');
