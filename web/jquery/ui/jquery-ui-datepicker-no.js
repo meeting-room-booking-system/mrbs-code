742c9d316e19f62867a7c6fef1c0d8e126563f17
Campbell Morrison <cimorrison@users.sourceforge.net>
2011-01-11 09:47:26 +0000

    - added explicit en-AU and en-NZ initialisations for jQuery UI datepicker.  (Meanwhile a ticket, #6828, has been raised requesting their inclusion in the standard package) - added a correction for the Norwegian initialisation for datepicker (pending discussions with the author of the original initialisation with a view to incorporating the changes in the standard package)

---
+++ web/jquery/ui/jquery-ui-datepicker-no.js
@@ -0,0 +1,25 @@
+/* $Id$ */
+/* Norwegian initialisation for the jQuery UI date picker plugin. */
+/* Modified version of the one written by Naimdjon Takhirov (naimdjon@gmail.com). */
+/* Based on input from Pål Monstad */
+jQuery(function($){
+    $.datepicker.regional['no'] = {
+		closeText: 'Lukk',
+        prevText: '&laquo;Forrige',
+		nextText: 'Neste&raquo;',
+		currentText: 'I dag',
+        monthNames: ['januar','februar','mars','april','mai','juni',
+        'juli','august','september','oktober','november','desember'],
+        monthNamesShort: ['jan','feb','mar','apr','mai','jun',
+        'jul','aug','sep','okt','nov','des'],
+		dayNamesShort: ['søn','man','tir','ons','tor','fre','lør'],
+		dayNames: ['søndag','mandag','tirsdag','onsdag','torsdag','fredag','lørdag'],
+		dayNamesMin: ['sø','ma','ti','on','to','fr','lø'],
+		weekHeader: 'Uke',
+        dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional['no']);
+});
