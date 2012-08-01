3186934b2656ceb2c4614323fac41d1781d526e5
John Beranek <jberanek@users.sourceforge.net>
2012-08-01 21:06:31 +0000

    Upgrade fixes as suggested in [bugs:#224]

--- web/upgrade/25/pgsql.sql
+++ web/upgrade/25/pgsql.sql
@@ -4,5 +4,9 @@
 -- series that has been modified, ie has entry_type=2, then the sequence number 
 -- cannot be 0.   We will set it to 1.
 
+ALTER TABLE %DB_TBL_PREFIX%entry ADD COLUMN saved_ts DATETIME;
+UPDATE %DB_TBL_PREFIX%entry SET saved_ts=timestamp;
 UPDATE %DB_TBL_PREFIX%entry 
-SET ical_sequence=1 WHERE ical_sequence=0 AND entry_type=2;
+  SET ical_sequence=1 WHERE ical_sequence=0 AND entry_type=2;
+UPDATE %DB_TBL_PREFIX%entry SET timestamp=saved_ts;
+ALTER TABLE %DB_TBL_PREFIX%entry DROP COLUMN saved_ts;
