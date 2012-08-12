0fda3ce01ebb4384bf2db7c2f686b44929ef148f
Campbell Morrison <cimorrison@users.sourceforge.net>
2012-08-12 19:48:31 +0000

    Fixed bug introduced in last revision where refreshing wasn't disabled if $refresh_rate equals zero

--- web/js/refresh.js.php
+++ web/js/refresh.js.php
@@ -18,33 +18,39 @@ var oldInitRefresh = init;
 init = function(args) {
   oldInitRefresh.apply(this, [args]);
 
-  var data = {ajax: 1, 
-              day: args.day,
-              month: args.month,
-              year: args.year,
-              room: args.room,
-              area: args.area};
-  if (args.timetohighlight !== undefined)
+  <?php
+  if (!empty($refresh_rate))
   {
-    data.timetohighlight = args.timetohighlight;
-  }
-  var table = $('table.dwm_main');
-  <?php // setTimeout not setInterval because the 'load' trigger restarts us ?>
-  window.setTimeout(function() {
-      $.post(args.page + '.php',
-             data,
-             function(result){
-                 <?php
-                 // (1) Empty the existing table in order to get rid of events
-                 // and data and prevent memory leaks (2) insert the updated 
-                 // table HTML and then (3) trigger a window load event so that 
-                 // the resizable bookings are re-created
-                 ?>
-                 table.empty()
-                 table.html(result);
-                 $(window).trigger('load');
-               },
-             'html');
-    }, <?php echo $refresh_rate * 1000 ?>);
-  
+    ?>
+    var data = {ajax: 1, 
+                day: args.day,
+                month: args.month,
+                year: args.year,
+                room: args.room,
+                area: args.area};
+    if (args.timetohighlight !== undefined)
+    {
+      data.timetohighlight = args.timetohighlight;
+    }
+    var table = $('table.dwm_main');
+    <?php // setTimeout not setInterval because the 'load' trigger restarts us ?>
+    window.setTimeout(function() {
+        $.post(args.page + '.php',
+               data,
+               function(result){
+                   <?php
+                   // (1) Empty the existing table in order to get rid of events
+                   // and data and prevent memory leaks (2) insert the updated 
+                   // table HTML and then (3) trigger a window load event so that 
+                   // the resizable bookings are re-created
+                   ?>
+                   table.empty()
+                   table.html(result);
+                   $(window).trigger('load');
+                 },
+               'html');
+      }, <?php echo $refresh_rate * 1000 ?>);
+    <?php
+  } // if (!empty($refresh_rate))
+  ?>
 }
