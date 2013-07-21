ef9a4221b411c6dc1cc76402bb9118d457e4dc84
Campbell Morrison <cimorrison@users.sourceforge.net>
2013-07-21 18:28:23 +0000

    Replaced use of deprecated jQuery .live() method

--- web/js/pending.js.php
+++ web/js/pending.js.php
@@ -41,15 +41,17 @@ init = function(args) {
     // Add a '-' control to the subtables and make them close on clicking it
     ?>
     maintable.find('table.sub th.control')
-             .text('-')
-             .live('click', function () {
-                  var nTr = $(this).closest('.table_container').parent().prev();
-                  var serial = $(this).parent().parent().parent().attr('id').replace('subtable_', '');
-                  $('#subtable_' + serial + '_wrapper').slideUp( function () {
-                      pendingTable.fnClose(nTr.get(0));
-                      nTr.show();
-                    });
-                });
+             .text('-');
+             
+    $(document).on('click', 'table.sub th.control', function () {
+        var nTr = $(this).closest('.table_container').parent().prev();
+        var serial = $(this).parent().parent().parent().attr('id').replace('subtable_', '');
+        $('#subtable_' + serial + '_wrapper').slideUp( function () {
+            pendingTable.fnClose(nTr.get(0));
+            nTr.show();
+          });
+      });
+      
     <?php
     // Detach all the subtables from the DOM (detach keeps a copy) so that they
     // don't appear, but so that we've got the data when we want to "open" a row
@@ -73,44 +75,44 @@ init = function(args) {
     // Set up a click event that "opens" the table row and inserts the subtable
     ?>
     maintable.find('td.control')
-             .text('+')
-             .live('click', function () {
-                  var nTr = $(this).parent();
-                  var serial = nTr.attr('id').replace('row_', '');
-                  var subtableId = 'subtable_' + serial;
-                  var subtable = subtables.find('#' + subtableId).parent().clone();                                
-                  var columns = [];          
-                  <?php
-                  // We want the columns in the main and sub tables to align.  So
-                  // find the widths of the main table columns and use those values
-                  // to set the widths of the subable columns.   [This doesn't work
-                  // 100% - I'm not sure why - but I have left the code in]
-                  ?>
-                  maintable.find('tr').eq(0).find('th').each(function(i){
-                      var def = {};
-                      switch (i)
-                      {
-                        case 0: <?php // expand control ?>
-                          def.bSortable = false;
-                          break;
-                        case 5: <?php // start-time ?>
-                          def.sType = "title-numeric";
-                          break;
-                      }
-                      def.sWidth = ($(this).innerWidth()) + "px";
-                      columns.push(def);
-                    });
+             .text('+');
+    $(document).on('click', 'td.control', function () {
+        var nTr = $(this).parent();
+        var serial = nTr.attr('id').replace('row_', '');
+        var subtableId = 'subtable_' + serial;
+        var subtable = subtables.find('#' + subtableId).parent().clone();                                
+        var columns = [];          
+        <?php
+        // We want the columns in the main and sub tables to align.  So
+        // find the widths of the main table columns and use those values
+        // to set the widths of the subable columns.   [This doesn't work
+        // 100% - I'm not sure why - but I have left the code in]
+        ?>
+        maintable.find('tr').eq(0).find('th').each(function(i){
+            var def = {};
+            switch (i)
+            {
+              case 0: <?php // expand control ?>
+                def.bSortable = false;
+                break;
+              case 5: <?php // start-time ?>
+                def.sType = "title-numeric";
+                break;
+            }
+            def.sWidth = ($(this).innerWidth()) + "px";
+            columns.push(def);
+          });
 
-                  nTr.hide();
-                  pendingTable.fnOpen(nTr.get(0), subtable.get(0), 'table_container');
+        nTr.hide();
+        pendingTable.fnOpen(nTr.get(0), subtable.get(0), 'table_container');
 
-                  $('#' + subtableId).dataTable({"bAutoWidth": false,
-                                                 "bPaginate": false,
-                                                 "sDom": 't',
-                                                 "aoColumns": columns});
+        $('#' + subtableId).dataTable({"bAutoWidth": false,
+                                       "bPaginate": false,
+                                       "sDom": 't',
+                                       "aoColumns": columns});
 
-                  $('#subtable_' + serial + '_wrapper').hide().slideDown();
-                });
+        $('#subtable_' + serial + '_wrapper').hide().slideDown();
+      });
                   
     <?php // Turn the table into a datatable ?>
     var tableOptions = {};
