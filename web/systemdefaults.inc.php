49131ed65d90063dd6144d68f11937fbd7f0f4bb
Campbell Morrison <cimorrison@users.sourceforge.net>
2013-11-27 09:46:24 +0000

    Corrected error in comment

--- web/systemdefaults.inc.php
+++ web/systemdefaults.inc.php
@@ -276,7 +276,7 @@ $max_per_interval_global['future'] = 100; // max 100 bookings in the future in t
 $max_booking_date_enabled = FALSE;
 $max_booking_date = "2012-07-23";  // Must be a string in the format "yyyy-mm-dd"
 
-// Set the latest date for which you can make a booking.    This can be useful if you
+// Set the earliest date for which you can make a booking.    This can be useful if you
 // want to set an absolute date, eg the beginning of term, before which bookings cannot be made.
 // If you want to set a relative date, eg no more than a week away, then this can be done
 // using the area settings.   Note that it is possible to have both a relative and absolute
