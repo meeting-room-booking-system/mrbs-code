aa2a89430b13e44d0494d555165c418a24767353
Campbell Morrison <cimorrison@users.sourceforge.net>
2014-10-14 14:04:17 +0000

    Changed spelling of the Spanish version of the short name for August in the datepicker from 'ogo' to 'ago'.   See SF Support Requests #625.   (Upgrading to jQuery UI 1.11 will have to wait as the 1.11 file structure has been changed).

--- web/jquery/ui/jquery-ui-i18n.MRBS.js
+++ web/jquery/ui/jquery-ui-i18n.MRBS.js
@@ -36,6 +36,27 @@ jQuery(function($){
   $.datepicker.regional['sr-RS-LATIN'] = $.datepicker.regional['sr-SR'];
   
   // Some extra language settings
-  $.datepicker.regional['en-IE'] = $.datepicker.regional['en-GB'];  // Ticket #9822 (new feature) 
+  $.datepicker.regional['en-IE'] = $.datepicker.regional['en-GB'];  // Ticket #9822 (new feature)
+  
+  // Overrides standard translation to fix a bug
+  $.datepicker.regional['es'] = {
+    closeText: 'Cerrar',
+    prevText: '&#x3C;Ant',
+    nextText: 'Sig&#x3E;',
+    currentText: 'Hoy',
+    monthNames: ['enero','febrero','marzo','abril','mayo','junio',
+    'julio','agosto','septiembre','octubre','noviembre','diciembre'],
+    monthNamesShort: ['ene','feb','mar','abr','may','jun',
+    'jul','ago','sep','oct','nov','dic'],
+    dayNames: ['domingo','lunes','martes','miércoles','jueves','viernes','sábado'],
+    dayNamesShort: ['dom','lun','mar','mié','juv','vie','sáb'],
+    dayNamesMin: ['D','L','M','X','J','V','S'],
+    weekHeader: 'Sm',
+    dateFormat: 'dd/mm/yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''};
+  $.datepicker.setDefaults($.datepicker.regional['es']);
 
 });
