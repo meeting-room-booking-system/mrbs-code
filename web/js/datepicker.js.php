e1080e879f237b378e49563f10cd101e3d8337a1
Campbell Morrison <cimorrison@users.sourceforge.net>
2014-11-27 16:41:29 +0000

    Converted to using new style jQuery UI datepicker regional settings

--- web/js/datepicker.js.php
+++ web/js/datepicker.js.php
@@ -12,81 +12,10 @@ if ($use_strict)
   echo "'use strict';\n";
 }
 
-// Set the default values for datepicker, including the default regional setting
+// Set the default values for datepicker
 ?>
 $(function() {
-  <?php
-  // We set the regional setting by setting locales in reverse order of priority.
-  // If you try and set a datepicker locale that doesn't exist, then nothing is
-  // changed and the regional setting stays as it was before.   The reverse order
-  // of priority is:
-  // - the MRBS default language
-  // - locales taken from the browser in increasing order of browser preference,
-  //   taking for each locale
-  //     - the language part only (in case the xx-YY localisation does not exist)
-  //     - the full locale
-  // - then, if automatic language changing is disabled, 
-  //      - the MRBS default language setting again
-  //      - the language part of the override_locale
-  //      - the full override_locale
-  // This algorithm is designed to ensure that datepicker is set to the closest
-  // available locale to that specified in the config file.   If automatic language
-  // changing is disabled, we fall back to a browser specified locale if the locale
-  // in the config file is not available in datepicker.
-  
-  $default_lang = locale_format($default_language_tokens, '-');
-  
-  // Note that we use [''] rather than dot notation for the regional settings because
-  // settings such as 'en-US' would break dot notation.
-  echo "$.datepicker.setDefaults($.datepicker.regional['$default_lang']);\n";
-  $datepicker_langs = get_language_qualifiers();
-  $datepicker_langs = alias_qualifiers($datepicker_langs);
-  asort($datepicker_langs, SORT_NUMERIC);
-  foreach ($datepicker_langs as $lang => $qual)
-  {
-    // Get the locale in the format that datepicker likes: language lower case
-    // and country upper case (xx-XX)
-    $datepicker_locale = locale_format($lang, '-');
-    // First we'll try and get the correct language and then we'll try and
-    // overwrite that with the correct country variant
-    if (strlen($datepicker_locale) > 2)
-    {
-      $datepicker_lang = substr($datepicker_locale, 0, 2);
-      echo "$.datepicker.setDefaults($.datepicker.regional['$datepicker_lang']);\n";
-    }
-    echo "$.datepicker.setDefaults($.datepicker.regional['$datepicker_locale']);\n";
-  }
-  if ($disable_automatic_language_changing)
-  {
-    // They don't want us to use the browser language, so we'll set the datepicker
-    // locale setting back to the default language (as a fall-back) and then we'll
-    // try and set it to the override_locale
-    echo "$.datepicker.setDefaults($.datepicker.regional['$default_lang']);\n";
-    if (!empty($override_locale))
-    {
-      if ($server_os == 'windows')
-      {
-        // If the server is running on Windows we'll have to try and translate the 
-        // Windows style locale back into an xx-YY locale
-        $datepicker_locale = array_search($override_locale, $lang_map_windows);
-      }
-      else
-      {
-        $datepicker_locale = $override_locale;
-      }
-      if (!empty($datepicker_locale))  // in case the array_search() returned FALSE
-      {
-        $datepicker_locale = locale_format($datepicker_locale, '-');
-        $datepicker_locale = substr($datepicker_locale, 0, 5);  // strip off anything after the country (eg charset)
-        $datepicker_lang = substr($datepicker_locale, 0, 2);
-        // First we'll try and get the correct language and then we'll try and
-        // overwrite that with the correct country variant
-        echo "$.datepicker.setDefaults($.datepicker.regional['$datepicker_lang']);\n";
-        echo "$.datepicker.setDefaults($.datepicker.regional['$datepicker_locale']);\n";
-      }
-    }
-  }
-  ?>
+
   $.datepicker.setDefaults({
     showOtherMonths: true,
     selectOtherMonths: true,
@@ -98,6 +27,7 @@ $(function() {
     altFormat: 'yy-mm-dd',
     onSelect: function(dateText, inst) {datepickerSelect(inst);}
   });
+  
 });
 
 
