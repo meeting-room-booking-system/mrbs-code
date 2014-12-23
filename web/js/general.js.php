67ff3c4dce77463f5c0e6e5fa0d5bc303f4fb58a
Campbell Morrison <cimorrison@users.sourceforge.net>
2014-12-23 21:35:36 +0000

    Fixed problem with alignment of floating header columns in IE

--- web/js/general.js.php
+++ web/js/general.js.php
@@ -31,12 +31,19 @@ var supportsDatalist = function supportsDatalist() {
   };
   
 <?php
+// Set up a cloned <thead> for use with floating headers
 ?>
 var createFloatingHeaders = function createFloatingHeaders(tables) {
     tables.each(function() {
       var originalHeader = $('thead', this),
           existingClone = $('.floatingHeader', this).first(),
-          clonedHeader;
+          clonedHeader,
+          nCols = originalHeader.find('th').length; <?php // assumes just one row in the header ?>
+      <?php
+      // We need to know if there's already a clone, because we only need to create one
+      // if there isn't one already (otherwise we'll end up with millions of them).  If
+      // there already is a clone, all we need to do is adjust its width.
+      ?>
       if (existingClone.length)
       {
         clonedHeader = existingClone;
@@ -46,11 +53,28 @@ var createFloatingHeaders = function createFloatingHeaders(tables) {
         clonedHeader = originalHeader.clone();
         clonedHeader.addClass('floatingHeader');
       }
+      <?php
+      // Now we need to set the width of the cloned header to equal the width of the original 
+      // header.   But we also need to set the widths of the header cells, because when they are
+      // not connected to the table body the constraints on the width are different and the columns
+      // may not line up.
+      //
+      // When calculating the width of the original cells we use getBoundingClientRect().width to
+      // avoid problems with IE which would otherwise round the widths.   But since
+      // getBoundingClientRect().width gives us the width including padding and borders (but not
+      // margins) we need to set the box-sizing model accordingly when setting the width.
+      //
+      // Note that these calculations assume border-collapse: separate.   If we were using 
+      // collapsed borders then we'd have to watch out for the fact that the borders are shared
+      // and then subtract half the border width (possibly on the inner cells only?).
+      ?>
       clonedHeader
           .css('width', originalHeader.width())
-          .find('th').width(function (i, val) {
-              return originalHeader.find('th').eq(i).width();    
-            });
+          .find('th')
+              .css('box-sizing', 'border-box')
+              .css('width', function (i, val) {
+                  return originalHeader.find('th').get(i).getBoundingClientRect().width;
+                });
       if (!existingClone.length)
       {
         clonedHeader.insertAfter(originalHeader);
@@ -58,7 +82,11 @@ var createFloatingHeaders = function createFloatingHeaders(tables) {
     });
   };
   
-  
+
+<?php
+// Make the floating header visible or hidden depending on the vertical scroll
+// position.  We also need to take account of horizontal scroll
+?>
 var updateTableHeaders = function updateTableHeaders(tables) {
     tables.each(function() {
 
@@ -251,6 +279,7 @@ init = function(args) {
   
   $('#Form1 input[type="submit"]').css('visibility', 'visible');
 
+  
   var floatingTables = $('table#day_main, table#week_main');
   createFloatingHeaders(floatingTables);
   
