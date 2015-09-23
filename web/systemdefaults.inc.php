478588f2cc79900d159c71fd24374296c34d31a9
Campbell Morrison <cimorrison@users.sourceforge.net>
2015-09-23 10:31:39 +0100

    Added support for qmail

--- web/systemdefaults.inc.php
+++ web/systemdefaults.inc.php
@@ -957,7 +957,7 @@ $mail_settings['username_suffix'] = '';
 // HOW TO EMAIL - BACKEND
 // ----------------------
 // Set the name of the backend used to transport your mails. Either 'mail',
-// 'smtp' or 'sendmail'. Default is 'mail'.
+// 'smtp', 'sendmail' or 'qmail'. Default is 'mail'.
 $mail_settings['admin_backend'] = 'mail';
 
 /*******************
