e273206ed686279c651df955acedd9623e3e5ff9
John Beranek <jberanek@users.sourceforge.net>
2015-10-14 20:25:28 +0100

    Updated some documentation.
    Updated PostgreSQL SQL files.
    
    Removed sample/test data, and any mention of them in the documentation.

--- destroy.pg.sql
+++ destroy.pg.sql
@@ -1,4 +1,3 @@
--- $Id$
 --
 -- MRBS table destruction script for PostgreSQL 7.0 or higher
 -- This exists because I can never remember the sequence name magic.
@@ -8,7 +7,6 @@
 -- lines below.
 --
 
-
 DROP TABLE mrbs_area;
 DROP SEQUENCE mrbs_area_id_seq;
 DROP TABLE mrbs_room;
@@ -17,3 +15,9 @@ DROP TABLE mrbs_entry;
 DROP SEQUENCE mrbs_entry_id_seq;
 DROP TABLE mrbs_repeat;
 DROP SEQUENCE mrbs_repeat_id_seq;
+DROP TABLE mrbs_users;
+DROP SEQUENCE mrbs_users_id_seq;
+DROP TABLE mrbs_variables;
+DROP SEQUENCE mrbs_variables_id_seq;
+DROP TABLE mrbs_zoneinfo;
+DROP SEQUENCE mrbs_zoneinfo_id_seq;
