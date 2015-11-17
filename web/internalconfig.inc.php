54337d5014807f19b272abde45e0b630c62e256d
Campbell Morrison <cimorrison@users.sourceforge.net>
2015-11-17 15:50:35 +0000

    Moved the PHP version check to happen earlier, otherwise MRBS will fail with obscure error messages if the PHP version is too low.   See SF Bugs #337.

--- web/internalconfig.inc.php
+++ web/internalconfig.inc.php
@@ -134,13 +134,6 @@ if ($dbsys == 'mysql')
  * Checking
  ********************************************************/
 
-// Check PHP version
-$min_PHP_version = '5.3.3';
-if (!function_exists('version_compare') || version_compare(PHP_VERSION, $min_PHP_version) < 0)
-{
-  die("MRBS requires PHP $min_PHP_version or above.   This server is running version " . PHP_VERSION . ".");
-}
-
 // Do some consistency checking of user settings from config.inc.php
 if ($enable_periods)
 {
