441749e1de1a2f7da792ff1067561e4918f16f3c
Campbell Morrison <cimorrison@users.sourceforge.net>
2016-09-14 13:25:31 +0100

    Fixed various bugs preventing mail being sent.

--- web/report.php
+++ web/report.php
@@ -937,11 +937,11 @@ function accumulate(&$row, &$count, &$hours, $report_start, $report_end,
     // the last period and midnight
     
     // Need to use the MRBS version of DateTime to get round a bug in modify()
-    // in PHP before 5.3.6
-    $startDate = new MRBS\DateTime();
+    // in PHP before 5.3.6.  As we are in the MRBS namespace we will get the MRBS version.
+    $startDate = new DateTime();
     $startDate->setTimestamp($report_start)->modify('12:00');
     
-    $endDate = new MRBS\DateTime();
+    $endDate = new DateTime();
     $endDate->setTimestamp($report_end)->modify('12:00');
     $endDate->sub(new DateInterval('P1D'));  // Go back one day because the $report_end is at 00:00 the day after
     $endDate->add(new DateInterval('PT' . $periods_per_day . 'M'));
