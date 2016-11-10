cb7a3d5d9fc6ffe6efeddf4a46ce07018bf055ec
Campbell Morrison <cimorrison@users.sourceforge.net>
2016-11-10 12:46:14 +0000

    Fixed bug that gave a double border on printing.  See SF Support Requests #1078.  Also fixed another bug at the same time - there was no border around bookings in the month view on printing.

--- web/css/mrbs-print.css.php
+++ web/css/mrbs-print.css.php
@@ -55,7 +55,7 @@ table#colour_key {margin-top: 1em}
 // Generate the classes to give the colour coding by booking type in the day/week/month views
 foreach ($color_types as $type => $col)
 {
-  echo "td.$type, .$type .celldiv {border: 2px solid $col}\n";         // used in the day and week views
+  echo "td.$type, #month_main div.$type, #colour_key td.$type {border: 2px solid $col}\n";         // used in the day and week views
 }
 
 // hide DataTable buttons in print
