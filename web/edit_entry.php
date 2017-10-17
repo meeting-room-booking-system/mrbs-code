e70f5c334ac46be207a7d73f12b32867ab23edec
Campbell Morrison <cimorrison@users.sourceforge.net>
2017-10-17 16:34:07 +0100

    Tidied up capitalisation when calling genDateSelector() [not a bug, as function names are case insensitive]

--- web/edit_entry.php
+++ web/edit_entry.php
@@ -298,7 +298,7 @@ function create_field_entry_start_date($disabled=FALSE)
   echo "<div id=\"div_start_date\">\n";
   echo "<label>" . get_vocab("start") . "</label>\n";
   echo "<div>\n"; // Needed so that the structure is the same as for the end date to help the JavaScript
-  gendateselector("start_", $date['mday'], $date['mon'], $date['year'], '', $disabled);
+  genDateSelector("start_", $date['mday'], $date['mon'], $date['year'], '', $disabled);
   echo "</div>\n";
 
   // Generate the live slot selector and all day checkbox
@@ -326,7 +326,7 @@ function create_field_entry_end_date($disabled=FALSE)
   echo "<label>" . get_vocab("end") . "</label>\n";
   // Don't show the end date selector if multiday is not allowed
   echo "<div" . (($multiday_allowed) ? '' : " style=\"visibility: hidden\"") . ">\n";
-  gendateselector("end_", $date['mday'], $date['mon'], $date['year'], '', $disabled);
+  genDateSelector("end_", $date['mday'], $date['mon'], $date['year'], '', $disabled);
   echo "</div>\n";
   
   // Generate the live slot selector
