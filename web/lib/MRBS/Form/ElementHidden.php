f4af2352426a6d4cca25d9a9660048142213930e
Campbell Morrison <cimorrison@users.sourceforge.net>
2017-10-20 18:18:19 +0100

    Fixed HTML5 validation error.  Made hidden inputs self-closing.

--- web/lib/MRBS/Form/ElementHidden.php
+++ web/lib/MRBS/Form/ElementHidden.php
@@ -8,6 +8,7 @@ class ElementHidden extends ElementInput
   public function __construct($name, $value)
   {
     parent::__construct('hidden');
+    $this->self_closing = true;
     $this->setAttributes(array('name'  => $name,
                                'value' => $value));
   }
