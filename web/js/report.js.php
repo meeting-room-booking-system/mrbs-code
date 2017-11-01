2591f5da3c26c1718d0bc5d3c8df6504153d8986
Campbell Morrison <cimorrison@users.sourceforge.net>
2017-11-01 20:33:22 +0000

    Added CRSF token and checking to report.php

--- web/js/report.js.php
+++ web/js/report.js.php
@@ -33,7 +33,6 @@ init = function(args) {
   ?>
   var summaryDiv = $('#div_summary'),
       summaryHead = summaryDiv.find('thead'),
-      queryString,
       tableOptions;
       
   summaryHead.find('tr:first th:odd').attr('colspan', '2');
@@ -67,6 +66,7 @@ init = function(args) {
       }
     }).trigger('change');
   
+  
   <?php
   // Turn the list of users into a dataTable
   ?>
@@ -77,13 +77,16 @@ init = function(args) {
   if (function_exists('json_encode'))
   {
     ?>
-    queryString = window.location.search;
-    if (queryString.length === 0)
-    {
-      queryString = '?phase=2';
-    }
-    queryString += '&ajax=1';
-    tableOptions.ajax = 'report.php' + queryString;
+
+    tableOptions.ajax = {url: 'report.php',
+                         type: 'POST', 
+                         processData: false,
+                         contentType: false,
+                         data: function() {
+                             var formdata = new FormData($('#report_form')[0]);
+                             formdata.append('ajax', '1');
+                             return formdata;
+                           } };
     <?php
   }
   // Add in a hidden input to the form so that we can tell if we are using DataTables
