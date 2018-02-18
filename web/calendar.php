aaba34248cbf910833f0249943282e419d53c301
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-02-18 16:50:32 +0000

    Restructured (and added missing file which should have been added earlier)

---
+++ web/calendar.php
@@ -0,0 +1,55 @@
+<?php
+namespace MRBS;
+
+require "defaultincludes.inc";
+
+
+function get_location_nav($view, $year, $month, $day, $area, $room)
+{
+  $html = '';
+  
+  $html .= "<nav id=\"location\">\n";
+  $html .= make_area_select_html($view, $year, $month, $day, $area);
+  
+  if ($view !== 'day')
+  {
+    $html .= make_room_select_html($view, $year, $month, $day, $area, $room);
+  }
+  
+  $html .= "</nav>\n";
+  
+  return $html;
+}
+
+
+function get_calendar_nav($view, $year, $month, $day, $area, $room)
+{
+  $html = '';
+  
+  $html .= "<nav id=\"calendar\">\n";
+  
+  $html .= get_location_nav($view, $year, $month, $day, $area, $room);
+  
+  
+  $html .= "</nav>\n";
+  
+  return $html;
+}
+
+
+// Get non-standard form variables
+$ajax = get_form_var('ajax', 'int');
+$view = get_form_var('view', 'string', isset($default_view) ? $default_view : 'day');
+
+// Check the user is authorised for this page
+if (!checkAuthorised($just_check = $ajax))
+{
+  exit;
+}
+
+// print the page header
+print_header($day, $month, $year, $area, isset($room) ? $room : null);
+
+echo get_calendar_nav($view, $year, $month, $day, $area, $room);
+
+print_footer();
