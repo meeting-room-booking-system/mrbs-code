62f402bf16c91f6c1160d3e70f2a5665a60e12e2
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-02-20 14:39:28 +0000

    Made the arrow links work

--- web/calendar.php
+++ web/calendar.php
@@ -5,6 +5,64 @@ require "defaultincludes.inc";
 require_once "functions_table.inc";
 
 
+// Gets the link to the next/previous day/week/month
+function get_adjacent_link($view, $year, $month, $day, $area, $room, $next=false)
+{
+  switch ($view)
+  {
+    case 'day':
+      // find the adjacent non-hidden day
+      $d = $day;
+      do
+      { 
+        $d += ($next) ? 1 : -1;
+        $time = mktime(12, 0, 0, $month, $d, $year);
+      }
+      while (is_hidden_day(date('w', $time)) && (abs($d - $day) < 7));  // break the loop if all days are hidden
+      break;
+    case 'week':
+      $time = mktime(12, 0, 0, $month, $day + (($next) ? 7 : -7), $year);
+      break;
+    case 'month':
+      $time = mktime(12, 0, 0, $month + (($next) ? 1 : -1), 1, $year);
+      // Keep the day information, but make sure it's a valid day in the new month
+      $d = min($day, date('t', $time));
+      $time = mktime(12, 0, 0, $month + (($next) ? 1 : -1), $d, $year);
+      break;
+    default:
+      throw new \Exception("Unknown view '$view'");
+      break;
+  }
+  
+  $date = getdate($time);
+  
+  $vars = array('view'  => $view,
+                'year'  => $date['year'],
+                'month' => $date['mon'],
+                'day'   => $date['mday'],
+                'area'  => $area,
+                'room'  => $room);
+  
+  return 'calendar.php?' . http_build_query($vars, '', '&amp;');
+}
+
+
+// Gets the link for today
+function get_today_link($view, $area, $room)
+{
+  $date = getdate();
+  
+  $vars = array('view'  => $view,
+                'year'  => $date['year'],
+                'month' => $date['mon'],
+                'day'   => $date['mday'],
+                'area'  => $area,
+                'room'  => $room);
+  
+  return 'calendar.php?' . http_build_query($vars, '', '&amp;');
+}
+
+
 function get_location_nav($view, $year, $month, $day, $area, $room)
 {
   $html = '';
@@ -75,14 +133,17 @@ function get_arrow_nav($view, $year, $month, $day, $area, $room)
       break;
   }
   
+  $link_prev = get_adjacent_link($view, $year, $month, $day, $area, $room, false);
+  $link_today = get_today_link($view, $area, $room);
+  $link_next = get_adjacent_link($view, $year, $month, $day, $area, $room, true);
+  
   $html .= "<nav class=\"arrow\">\n";
-  $html .= "<a class=\"prev\" title=\"$title_prev\" href=\"#\"></a>";  // Content will be filled in by CSS
-  $html .= "<a href=\"#\">" . get_vocab('today') . "</a>";
-  $html .= "<a class=\"next\" title=\"$title_next\"  href=\"#\"></a>";  // Content will be filled in by CSS
+  $html .= "<a class=\"prev\" title=\"$title_prev\" href=\"" . $link_prev . "\"></a>";  // Content will be filled in by CSS
+  $html .= "<a href=\"" . $link_today . "\">" . get_vocab('today') . "</a>";
+  $html .= "<a class=\"next\" title=\"$title_next\" href=\"" . $link_next . "\"></a>";  // Content will be filled in by CSS
   $html .= "</nav>";
   
   return $html;
-  
 }
 
 
