c508db978be955b9225c21e64c951ca8f59d07c4
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-03-04 17:18:37 +0000

    Week numbers are now used, if configured, on non-mobile devices.

--- web/js/datepicker.js.php
+++ web/js/datepicker.js.php
@@ -20,27 +20,12 @@ if ($use_strict)
 var oldInitDatepicker = init;
 init = function() {
   oldInitDatepicker.apply(this);
-  
-  $.datepicker.setDefaults({
-      showOtherMonths: true,
-      selectOtherMonths: true,
-      changeMonth: true,
-      changeYear: true,
-      duration: 'fast',
-      showWeek: <?php echo ($view_week_number) ? 'true' : 'false' ?>,
-      firstDay: <?php echo $weekstarts ?>,
-      altFormat: 'yy-mm-dd'
-    });
     
   <?php
-  // Set up datepickers.  We convert all inputs of type 'date' into
-  // jQueryUI datepickers.   In the future we might want to do something a bit
-  // more sophisticated and only convert the inputs for some browsers, because
-  // the native date controls on some modern browsers, eg Chrome mobile, are
-  // better than the jQueryUI datepicker.
-  ?>
+  // Set up datepickers.  We convert all inputs of type 'date' into flatpickr
+  // datepickers.  Note that by default flatpickr will use the native datepickers
+  // on mobile devices because they are generally better.
   
-  <?php
   // Localise the flatpickr
   if (null !== ($flatpickr_lang_file = get_flatpickr_lang_file('flatpickr/l10n')))
   {
@@ -48,7 +33,12 @@ init = function() {
     echo 'flatpickr.localize(flatpickr.l10ns.' . substr($flatpickr_lang_file, 0, -3) . ');';
   }
   ?>
-  flatpickr('input[type="date"]', {
+  
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+      
+  var config = {
       locale: {firstDayOfWeek: <?php echo $weekstarts ?>},
       onChange: function(selectedDates, dateStr, instance) {
         var submit = $(this.element).data('submit');
@@ -61,65 +51,19 @@ init = function() {
           $(this.element).change();
         }
       }
-    });
+    };
+    
+  if (!isMobile)
+  {
+    <?php
+    // Setting weekNumbers causes flatpickr not to use the native datepickers on mobile
+    // devices.  As these are generally better than flatpickr's, it's probably better
+    // to have the native datepicker and do without the week numbers.
+    ?>
+    config.weekNumbers = <?php echo ($view_week_number) ? 'true' : 'false' ?>;
+  }
+    
+  flatpickr('input[type="date"]', config);
   
-  $('input[type="date"]').each(function() {
-      return;
-      $(this).flatpickr({
-        <?php
-        // Note that the following settings cause flatpickr not to use the native
-        // datepicker on mobile devices.  As it's probably better to use the native
-        // datepickers, we don't use these settings.   (We could get more 
-        // sophisticated and only use these settings on non-mobile devices).
-        //    weekNumbers
-        ?>
-      });
-      return;
-      
-      
-      var input = $(this),
-          thisDate = input.val(),
-          thisName = input.attr('name'),
-          altId = thisName + '_alt';
-      
-      <?php
-      // Create a hidden field, which will be the alt field, that will
-      // hold the date value in the standard format.
-      ?>
-      $('<input>').attr('type', 'hidden')
-                  .attr('id', altId)
-                  .attr('name', thisName)
-                  .val(thisDate)
-                  .insertAfter(input);
-          
-      input.attr('type','text')
-           .removeAttr('name')
-           .addClass('date')
-           .datepicker({altField: '#' + altId,
-                        onSelect: function(dateText, inst) {
-                            var submit = $(this).data('submit');
-                            if (submit)
-                            {
-                              $('#' + submit).submit();
-                            }
-                            else
-                            {
-                              $(this).change();
-                            }
-                          }
-                        });
-           
-      <?php
-      // Initialise the date in the field.   Our date is in yy-mm-ddSelect
-      // format, so we have to save the current datepicker format,
-      // change the format, set the date and then restore the old format.
-      // (Note: the other way of doing it by using a Date object presents
-      // timezone complications).
-      ?>
-      var dateFormat = input.datepicker('option', 'dateFormat');
-      input.datepicker('option', 'dateFormat', 'yy-mm-dd');
-      input.datepicker('setDate', new Date(thisDate));
-      input.datepicker('option', 'dateFormat', dateFormat);
-    });
 };
 
