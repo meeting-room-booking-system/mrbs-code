7e0d0602b698035f0bbc7feb919fb43a2eaaa26a
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-03-21 12:48:14 +0000

    Made the bottom navigation bar only visible if the top one isn't.

---
+++ web/js/index.js.php
@@ -0,0 +1,41 @@
+<?php
+namespace MRBS;
+
+require '../defaultincludes.inc';
+
+http_headers(array("Content-type: application/x-javascript"),
+             60*30);  // 30 minute expiry
+
+if ($use_strict)
+{
+  echo "'use strict';\n";
+}
+
+
+// Only show the bottom nav bar if no part of the top one is visible.
+?>
+var checkNav = function() {
+    if ($('nav.main_calendar').eq(0).visible(true))
+    {
+      $('nav.main_calendar').eq(1).hide();
+    }
+    else
+    {
+      $('nav.main_calendar').eq(1).show();
+    }
+  };
+
+<?php
+// =================================================================================
+
+// Extend the init() function 
+?>
+
+var oldInitIndex = init;
+init = function(args) {
+  
+  oldInitIndex.apply(this, [args]);
+  
+  $(window).scroll(checkNav);
+  $(window).resize(checkNav);
+};
