6980f3f0adac76aeceb07f8579090dd6f927071d
John Beranek <jberanek@users.sourceforge.net>
2018-03-31 08:53:24 +0100

    Fixed typo in upgrade script comment.

--- web/upgrade/55/mysql.sql
+++ web/upgrade/55/mysql.sql
@@ -1,6 +1,6 @@
 #
 # Change all tables to use utf8mb4 charset instead of the flawed utf8 charset.
-# The 'zoneinfo' table still uses utf8 so the uniqie index on the 255 character 'zoneinfo'
+# The 'zoneinfo' table still uses utf8 so the unique index on the 255 character 'zoneinfo'
 # column doesn't go over the 767 byte limit on MySQL 'COMPACT' format tables.
 
 ALTER TABLE %DB_TBL_PREFIX%area CONVERT TO CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
