3f007d82b3116d3d29570f67dde8834cd5281a08
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-04-15 18:09:44 +0100

    Fixed bug causing edit_entry_handler to go to today when finished rather than the day of the srtart of the booking.

--- web/edit_entry_handler.php
+++ web/edit_entry_handler.php
@@ -625,7 +625,9 @@ if (!isset($start_day) || !isset($start_month) || !isset($start_year) || !checkd
 }
 
 // Now construct the new query string
-$returl .= "?year=$year&month=$month&day=$day";
+$returl .= '?year='  . $start_year .
+           '&month=' . $start_month .
+           '&day='   . $start_day;
 
 // If the old sticky room is one of the rooms requested for booking, then don't change the sticky room.
 // Otherwise change the sticky room to be one of the new rooms.
