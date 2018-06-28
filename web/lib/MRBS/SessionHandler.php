d19a076dbfb94822958b6587cdb19e34f3659995
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-06-28 14:30:25 +0100

    First implementation of SessionHandler

---
+++ web/lib/MRBS/SessionHandler.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace MRBS;
+
+class SessionHandler implements \SessionHandlerInterface
+{
+  
+  public function __construct()
+  {
+    session_set_save_handler(
+        array($this, 'open'),
+        array($this, 'close'),
+        array($this, 'read'),
+        array($this, 'write'),
+        array($this, 'destroy'),
+        array($this, 'gc')
+      );
+      
+    register_shutdown_function('session_write_close');
+    session_start();
+  }
+    
+    
+  public function open($save_path , $session_name)
+  {
+    return true;
+  }
+
+  
+  public function close()
+  {
+    return true;
+  }
+
+  
+  public function read($session_id)
+  {
+    global $tbl_sessions;
+    
+    $sql = "SELECT data
+              FROM $tbl_sessions
+             WHERE id=:id
+             LIMIT 1";
+             
+    $result = db()->query1($sql, array(':id' => $session_id));
+    
+    return ($result === -1) ? '' : $result;
+  }
+
+  
+  public function write($session_id , $session_data)
+  {
+    global $tbl_sessions;
+    
+    $sql = "UPDATE $tbl_sessions
+               SET data=:data, access=:access
+             WHERE id=:id";
+    
+    $sql_params = array(':id' => $session_id,
+                        ':data' => $session_data,
+                        ':access' => time());
+                            
+    $rows = db()->command($sql, $sql_params);
+    
+    if ($rows === 1)
+    {
+      return true;
+    }
+    
+    if ($rows === 0)
+    {
+      // The id didn't exist so we have to INSERT it (we couldn't use
+      // REPLACE INTO because we have to cater for both MySQL and PostgreSQL)
+      $sql = "INSERT INTO $tbl_sessions
+                          (id, data, access)
+                   VALUES (:id, :data, :access)";
+      
+      $rows = db()->command($sql, $sql_params);
+      
+      if ($rows === 1)
+      {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+
+  
+  public function destroy($session_id)
+  {
+    global $tbl_sessions;
+    
+    $sql = "DELETE FROM $tbl_sessions WHERE id=:id";
+    $rows = $rows = db()->command($sql, $sql_params);
+    return ($rows === 1);
+  }
+
+  
+  public function gc($maxlifetime)
+  {
+    global $tbl_sessions;
+    
+    $sql = "DELETE FROM $tbl_sessions WHERE access<:old"; 
+    db()->command($sql, array(':old' => time() - $maxlifetime));  
+    return true;  // An exception will be thrown on error
+  }
+}
