84a97d55d217a1b3458dbd74f8a15c3b9a7d2d07
Campbell Morrison <cimorrison@users.sourceforge.net>
2018-11-02 14:56:14 +0000

    Removed the redundant "non_js" class

--- web/js/set_class.js.php
+++ web/js/set_class.js.php
@@ -8,8 +8,6 @@ http_headers(array("Content-type: application/x-javascript"),
              
              
 // Add a class of "js" so that we know if we're using JavaScript or not
-// and remove the non_js class (it's sometimes useful to know that we're
-// not running JavaScript)
 ?>
 
-$('body').addClass('js').removeClass('non_js');
+$('body').addClass('js');
