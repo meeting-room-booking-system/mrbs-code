3bf5cad6de3c4d97e12a1ff9499fb96256360bf3
Campbell Morrison <cimorrison@users.sourceforge.net>
2019-01-03 16:34:20 +0000

    Completed SessionPhp

--- web/lib/MRBS/Session/SessionPhp.php
+++ web/lib/MRBS/Session/SessionPhp.php
@@ -1,8 +1,37 @@
 <?php
 namespace MRBS\Session;
 
+// Uses PHP's built-in session handling
+
 class SessionPhp extends SessionWithLogin
 {
+  
+  public function __construct()
+  {
+    global $auth;
+    
+    parent::__construct();
+    
+    // Check to see if we've been inactive for longer than allowed and if so logout
+    // the user
+    if (!empty($auth['session_php']['inactivity_expire_time']))
+    {
+      if (isset($_SESSION['LastActivity']) &&
+          ((time() - $_SESSION['LastActivity']) > $auth['session_php']['inactivity_expire_time']))
+      {
+        $this->logoffUser();
+      }
+      // Ajax requests don't count as activity, unless it's the special Ajax request used
+      // to record client side activity
+      $ajax = \MRBS\get_form_var('ajax', 'int');
+      $activity = \MRBS\get_form_var('activity', 'int');
+      if ($activity || !$ajax || !isset($_SESSION['LastActivity']))
+      {
+        $_SESSION['LastActivity'] = time();
+      }
+    }
+  }
+  
   
   public function getUsername()
   {
