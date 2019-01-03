3de09636e7787b1fe16ac25debe788ddea48e198
Campbell Morrison <cimorrison@users.sourceforge.net>
2019-01-03 15:21:55 +0000

    Implemented logon in SessionCas

--- web/lib/MRBS/Session/SessionCas.php
+++ web/lib/MRBS/Session/SessionCas.php
@@ -31,4 +31,38 @@ class SessionCas extends SessionWithLogin
       );
   }
   
+  
+  public function processForm()
+  {
+    if (isset($this->form['action']))
+    {
+      // Target of the form with sets the URL argument "action=QueryName".
+      if ($this->form['action'] == 'QueryName')
+      {
+        phpCAS::forceAuthentication();
+      }
+      
+      // Target of the form with sets the URL argument "action=SetName".
+      // Will eventually return to URL argument "target_url=whatever".
+      if ($this->form['action'] == 'SetName')
+      {
+        // If we're going to do something then check the CSRF token first
+        Form::checkToken();
+        
+        // You should only get here using CAS authentication after clicking the logoff
+        // link, no matter what the value of the form parameters.
+        $this->logoffUser();
+        
+        header ('Location: ' . $this->form['target_url']); /* Redirect browser to initial page */
+        exit;
+      }
+    }
+  }
+  
+  
+  protected function logoffUser()
+  {
+    phpCAS::logout();
+  }
+  
 }
