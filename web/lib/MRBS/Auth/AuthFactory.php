80607a930608f791fac595cd460cf0872ef8aa38
Campbell Morrison <cimorrison@users.sourceforge.net>
2019-01-05 18:53:22 +0000

    Fixed comment

--- web/lib/MRBS/Auth/AuthFactory.php
+++ web/lib/MRBS/Auth/AuthFactory.php
@@ -7,7 +7,7 @@ class AuthFactory
   public static function create($type)
   
   {
-    // Transform the session type from lowercase_separated to LowercaseSeparated
+    // Transform the authentication type from lowercase_separated to LowercaseSeparated
     $parts = explode('_', $type);
     $parts = array_map('ucfirst', $parts);   
     $class = __NAMESPACE__  . "\\Auth" . implode('', $parts);
