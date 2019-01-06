f346c9d0f867f4b005ee54e9f79b7db99a7db0cd
Campbell Morrison <cimorrison@users.sourceforge.net>
2019-01-06 13:58:30 +0000

    Added AuthConfig class

--- web/lib/MRBS/Auth/Auth.php
+++ web/lib/MRBS/Auth/Auth.php
@@ -10,6 +10,44 @@ abstract class Auth
   {
     $user = new User($username);
     $user->display_name = $username;
+    $user->email = self::getDefaultEmail($username);
+    
     return $user;
+  }
+  
+  
+  // Gets the default email address for $username.   Returns null if one can't be found
+  private static function getDefaultEmail($username)
+  {
+    global $mail_settings;
+    
+    if (!isset($username) || $username === '')
+    {
+      return null;
+    }
+    
+    $email = $username;
+    
+    // Remove the suffix, if there is one
+    if (isset($mail_settings['username_suffix']) && ($mail_settings['username_suffix'] !== ''))
+    {
+      $suffix = $mail_settings['username_suffix'];
+      if (substr($email, -strlen($suffix)) === $suffix)
+      {
+        $email = substr($email, 0, -strlen($suffix));
+      }
+    }
+    
+    // Add on the domain, if there is one
+    if (isset($mail_settings['domain']) && ($mail_settings['domain'] !== ''))
+    {
+      // Trim any leading '@' character. Older versions of MRBS required the '@' character
+      // to be included in $mail_settings['domain'], and we still allow this for backwards
+      // compatibility.
+      $domain = ltrim($mail_settings['domain'], '@');
+      $email .= '@' . $domain;
+    }
+    
+    return $email;
   }
 }
