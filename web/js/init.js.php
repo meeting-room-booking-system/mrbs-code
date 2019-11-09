2aa3709bb442c39edfe9e910f4f55bf39115fc59
Campbell Morrison <cimorrison@users.sourceforge.net>
2019-11-09 10:31:16 +0000

    Eliminated flickering when navigating between pages in the calendar view in some browsrs, eg Firefox.

--- web/js/init.js.php
+++ web/js/init.js.php
@@ -7,5 +7,7 @@ http_headers(array("Content-type: application/x-javascript"),
              60*30);  // 30 minute expiry
              
              
-// Add a class of "js" so that we know if we're using JavaScript or not ?>
-$('body').addClass('js');
+// Add a class of "js" so that we know if we're using JavaScript or not.
+// jQuery hasn't been loaded yet, so use JavaScript ?>
+var html = document.getElementsByTagName('html')[0];
+html.classList.add('js');
