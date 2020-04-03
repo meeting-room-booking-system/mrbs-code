e6947832afb6386876b48c0fb916e7bbbaaf75e0
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-04-03 15:59:20 +0100

    Fixed bug when the user is not a Joomla user

--- web/lib/MRBS/Auth/AuthJoomla.php
+++ web/lib/MRBS/Auth/AuthJoomla.php
@@ -9,43 +9,48 @@ require_once MRBS_ROOT . '/auth/cms/joomla.inc';
 
 class AuthJoomla extends Auth
 {
-  
+
   public function getUser($username=null)
   {
     if ($username === '')
     {
       return null;
     }
-    
+
     $joomla_user = JFactory::getUser($username);
 
+    if ($joomla_user === false)
+    {
+      return new User($username);
+    }
+    
     if ($joomla_user->guest)
     {
       return null;
     }
-    
+
     $user = new User($joomla_user->username);
     $user->display_name = $joomla_user->name;
     $user->email = $joomla_user->email;
     $user->level = self::getUserLevel($joomla_user);
-    
+
     return $user;
   }
-  
-  
+
+
   private static function getUserLevel(\MRBS\JUser $joomla_user)
   {
     global $auth;
-    
+
     // User not logged in, user level '0'
     if ($joomla_user->guest)
     {
       return 0;
     }
-    
+
     // Otherwise get the user's access levels
     $authorised_levels = $joomla_user->getAuthorisedViewLevels();
-    
+
     // Check if they have admin access
     if (isset($auth['joomla']['admin_access_levels']))
     {
@@ -55,7 +60,7 @@ class AuthJoomla extends Auth
         return 2;
       }
     }
-    
+
     // Check if they have user access
     if (isset($auth['joomla']['user_access_levels']))
     {
@@ -69,5 +74,5 @@ class AuthJoomla extends Auth
     // Everybody else is access level '0'
     return 0;
   }
-  
+
 }
