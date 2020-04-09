d449c94946d89646007da4f7a05186d0c6a39350
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-04-09 16:32:49 +0100

    Shifted authGetUsernames into the Auth class

--- web/lib/MRBS/Auth/AuthDb.php
+++ web/lib/MRBS/Auth/AuthDb.php
@@ -5,20 +5,20 @@ use MRBS\User;
 
 class AuthDb extends Auth
 {
-  
+
   public function getUser($username)
   {
     global $tbl_users;
-    
+
     $sql = "SELECT * FROM $tbl_users WHERE name=:name LIMIT 1";
     $result = \MRBS\db()->query($sql, array(':name' => $username));
-    
+
     // The username doesn't exist - return NULL
     if ($result->count() === 0)
     {
       return null;
     }
-    
+
     // The username does exist - return a User object
     $data = $result->next_row_keyed();
 
@@ -34,11 +34,22 @@ class AuthDb extends Auth
       }
       $user->$key = $value;
     }
-    
+
     // We don't yet have a displayname field in the 'db' scheme, so make it the username
     $user->display_name = $user->username;
-    
+
     return $user;
+  }
+
+
+  // Return an array of users, indexed by 'username' and 'display_name'
+  public function getUsernames()
+  {
+    global $tbl_users;
+
+    $res = \MRBS\db()->query("SELECT name AS username, name AS display_name FROM $tbl_users ORDER BY name");
+
+    return $res->all_rows_keyed();
   }
 
 
@@ -47,5 +58,5 @@ class AuthDb extends Auth
   {
     return true;
   }
-  
+
 }
