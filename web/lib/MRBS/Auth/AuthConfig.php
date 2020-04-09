f3f8258ebe87f23c2d9013d3dc413ecdd8da0c6b
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-04-09 15:50:56 +0100

    Shifted authGetUsernames into the Auth class

--- web/lib/MRBS/Auth/AuthConfig.php
+++ web/lib/MRBS/Auth/AuthConfig.php
@@ -4,5 +4,22 @@ namespace MRBS\Auth;
 
 class AuthConfig extends Auth
 {
-  
+  // Return an array of users, indexed by 'username' and 'display_name'
+  public function getUsernames()
+  {
+    global $auth;
+
+    $result = array();
+
+    foreach ($auth['user'] as $user => $password)
+    {
+      $result[] = array('username'     => $user,
+        'display_name' => $user);
+    }
+
+    // Need to sort the users
+    self::sortUsers($result);
+
+    return $result;
+  }
 }
