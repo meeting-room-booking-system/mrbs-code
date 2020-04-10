6c47ffc22c74de7b1667ed21f236fc1963539f7d
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-04-10 11:35:09 +0100

    Shifted authValidateUser() into the Auth class

--- web/lib/MRBS/Auth/AuthConfig.php
+++ web/lib/MRBS/Auth/AuthConfig.php
@@ -4,6 +4,41 @@ namespace MRBS\Auth;
 
 class AuthConfig extends Auth
 {
+  /* authValidateUser($user, $pass)
+   *
+   * Checks if the specified username/password pair are valid
+   *
+   * $user  - The user name
+   * $pass  - The password
+   *
+   * Returns:
+   *   false    - The pair are invalid or do not exist
+   *   string   - The validated username
+   */
+  public function validateUser($user, $pass)
+  {
+    global $auth;
+
+    // Check if we do not have a username/password
+    if(!isset($user) || !isset($pass) || strlen($pass)==0)
+    {
+      return false;
+    }
+
+    if ((isset($auth["user"][$user]) &&
+        ($auth["user"][$user] == $pass)
+      ) ||
+      (isset($auth["user"][\MRBS\utf8_strtolower($user)]) &&
+        ($auth["user"][\MRBS\utf8_strtolower($user)] == $pass)
+      ))
+    {
+      return $user;    // User validated
+    }
+
+    return false;      // User unknown or password invalid
+  }
+
+
   // Return an array of users, indexed by 'username' and 'display_name'
   public function getUsernames()
   {
