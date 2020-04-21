96a95f23a9cc25aec3f1eb845a13b3c360ade66d
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-04-21 16:02:31 +0100

    Added comments

--- web/ajax/usernames.php
+++ web/ajax/usernames.php
@@ -4,6 +4,14 @@ namespace MRBS;
 // Returns an object containing all the usernames available for use by the Select2
 // tool on the edit_entry page.
 
+// There are improvements that could be made:
+//   1.  We could arrange for the usernames to be returned in a Select2 friendly
+//       format so that we don't have to iterate through them here.
+//   2.  We could take advantage of the Select2 'term' parameter to limit the number
+//       of results that have to be returned.
+//   3.  We could take advantage of pagination.
+// See https://select2.org/data-sources/ajax fot more details
+
 use MRBS\Form\Form;
 
 require '../defaultincludes.inc';
