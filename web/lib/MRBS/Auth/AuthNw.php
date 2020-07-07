807ebaf28c40c96ed9265ebee1874e737acaed81
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-07-07 15:15:09 +0100

    Converted auth_nw to a class

---
+++ web/lib/MRBS/Auth/AuthNw.php
@@ -0,0 +1,51 @@
+<?php
+namespace MRBS\Auth;
+
+use MRBS\User;
+
+class AuthNw extends Auth
+{
+  /* authValidateUser($user, $pass)
+   *
+   * Checks if the specified username/password pair are valid
+   *
+   * $user  - The user name
+   * $pass  - The password
+   *
+   * Returns:
+   *   false    - The pair are invalid or do not exist
+   *   string   - The validated username
+   */
+  public function validateUser($user, $pass)
+  {
+    global $auth;
+
+    // Check if we do not have a username/password
+    if (empty($user) || empty($pass))
+    {
+      return false;
+    }
+
+    // Generate the command line
+    $cmd = $auth["prog"] . " -S " . $auth["params"] . " -U '$user'";
+
+    // Run the program, sending the password to stdin.
+    $p = popen($cmd, "w");
+
+    if (!$p)
+    {
+      return false;
+    }
+
+    fputs($p, $pass);
+    
+    if (pclose($p) == 0)
+    {
+      return $user;
+    }
+
+    // return failure
+    return false;
+  }
+
+}
