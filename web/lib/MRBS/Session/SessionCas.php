cf5b5ce895d0a702e44de5345bff22072e59b165
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-07-09 15:35:25 +0100

    Converted auth_cas to a class

--- web/lib/MRBS/Session/SessionCas.php
+++ web/lib/MRBS/Session/SessionCas.php
@@ -7,23 +7,23 @@ use MRBS\Form\Form;
 
 class SessionCas extends SessionWithLogin
 {
-  
+
   public function authGet($target_url=null, $returl=null, $error=null, $raw=false)
   {
     // Useless Method - CAS does it all
   }
-  
-  
-  public function getUsername()
+
+
+  public function getCurrentUser()
   {
-    return (phpCAS::isAuthenticated()) ? phpCAS::getUser() : null;
+    return (phpCAS::isAuthenticated()) ? \MRBS\auth()->getUser(phpCAS::getUser()) : null;
   }
-  
-  
+
+
   public function getLogonFormParams()
   {
     $target_url = \MRBS\this_page(true);
-    
+
     return array(
         'action' => $target_url,
         'method' => 'post',
@@ -31,8 +31,8 @@ class SessionCas extends SessionWithLogin
                                   'action'     => 'QueryName')
       );
   }
-  
-  
+
+
   public function processForm()
   {
     if (isset($this->form['action']))
@@ -42,27 +42,27 @@ class SessionCas extends SessionWithLogin
       {
         phpCAS::forceAuthentication();
       }
-      
+
       // Target of the form with sets the URL argument "action=SetName".
       // Will eventually return to URL argument "target_url=whatever".
       if ($this->form['action'] == 'SetName')
       {
         // If we're going to do something then check the CSRF token first
         Form::checkToken();
-        
+
         // You should only get here using CAS authentication after clicking the logoff
         // link, no matter what the value of the form parameters.
         $this->logoffUser();
-        
+
         \MRBS\location_header($this->form['target_url']); // Redirect browser to initial page
       }
     }
   }
-  
-  
+
+
   protected function logoffUser()
   {
     phpCAS::logout();
   }
-  
+
 }
