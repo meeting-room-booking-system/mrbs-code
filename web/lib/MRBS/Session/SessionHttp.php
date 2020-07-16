4de877c55110f78f1870396ef6003004a1d61f57
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-07-16 16:17:46 +0100

    Fixed problem caused by trailing spaces in the username in the 'http' session scheme.

--- web/lib/MRBS/Session/SessionHttp.php
+++ web/lib/MRBS/Session/SessionHttp.php
@@ -6,45 +6,53 @@ namespace MRBS\Session;
 
 class SessionHttp extends SessionWithLogin
 {
-  
+
   public function authGet($target_url=null, $returl=null, $error=null, $raw=false)
   {
     global $auth;
-    
+
     header("WWW-Authenticate: Basic realm=\"$auth[realm]\"");
     header("HTTP/1.0 401 Unauthorized");
   }
-  
-  
+
+
   public function getCurrentUser()
   {
     global $server;
-    
-    if (!isset($server['PHP_AUTH_USER']) || ($server['PHP_AUTH_USER'] === ''))
+
+    if (!isset($server['PHP_AUTH_USER']))
     {
       return null;
     }
-    
-    if (\MRBS\auth()->validateUser($server['PHP_AUTH_USER'], self::getAuthPassword()) === false)
+
+    // Trim any whitespace because PHP_AUTH_USER can contain it.
+    $php_auth_user = trim($server['PHP_AUTH_USER']);
+
+    if ($php_auth_user === '')
     {
       return null;
     }
-    
-    return \MRBS\auth()->getUser($server['PHP_AUTH_USER']);
+
+    if (\MRBS\auth()->validateUser($php_auth_user, self::getAuthPassword()) === false)
+    {
+      return null;
+    }
+
+    return \MRBS\auth()->getUser($php_auth_user);
   }
-  
-  
+
+
   public function getLogoffFormParams()
   {
     // Just return NULL - you can't logoff
     // (well, there are ways of achieving a logoff but we haven't implemented them)
   }
-  
-  
+
+
   private static function getAuthPassword()
   {
     global $server;
-    
+
     return (isset($server['PHP_AUTH_PW'])) ? $server['PHP_AUTH_PW'] : null;
   }
 }
