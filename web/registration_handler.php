e6d9fa938e74f37acd94a7e965d97cce2c8adb46
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-07-22 14:24:38 +0100

    Added a check that there are spare places

--- web/registration_handler.php
+++ web/registration_handler.php
@@ -6,6 +6,7 @@ require "defaultincludes.inc";
 use MRBS\Form\Form;
 
 
+// Cancel a user's registration
 function cancel($username, $event_id)
 {
   $sql = "DELETE FROM " . _tbl('participants') . "
@@ -20,18 +21,38 @@ function cancel($username, $event_id)
 }
 
 
+// Register a user for an event
 function register($username, $event_id)
 {
-  $sql = "INSERT INTO " . _tbl('participants') . " (entry_id, username, registered)
+  // Obtain a lock to make sure no one else registers after we've checked that there
+  // are spare places
+  db()->mutex_lock(_tbl('participants'));
+
+  $data = get_booking_info($event_id, false);
+
+  // Check that registration is allowed ...
+  if (!empty($data['allow_registration']))
+  {
+    // ... and that there are spare places
+    $n_registered = count($data['registrants']);
+    if (empty($data['enable_registrant_limit']) ||
+      ($data['registrant_limit'] > $n_registered))
+    {
+      $sql = "INSERT INTO " . _tbl('participants') . " (entry_id, username, registered)
                VALUES (:entry_id, :username, :registered)";
 
-  $sql_params = array(
-    ':entry_id' => $event_id,
-    ':username' => $username,
-    ':registered' => time()
-  );
+      $sql_params = array(
+          ':entry_id' => $event_id,
+          ':username' => $username,
+          ':registered' => time()
+        );
 
-  db()->command($sql, $sql_params);
+      db()->command($sql, $sql_params);
+    }
+  }
+
+  // Release the lock
+  db()->mutex_lock(_tbl('participants'));
 }
 
 // Check the CSRF token.
