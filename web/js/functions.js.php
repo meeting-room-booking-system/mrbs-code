7043b0542d54ecd112ce04c1e2fb6793b14aeee1
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-08-13 17:03:27 +0100

    Made the datalist work on mobiles

--- web/js/functions.js.php
+++ web/js/functions.js.php
@@ -35,8 +35,9 @@ jQuery.fn.extend({
 
   <?php
   // Turn a select element into a fancy Select2 field control.  This wrapper function also
-  // (a) only does anything if we are not on a mobile device, because the native select
-  //     elements on mobile devices tend to be better.
+  // (a) only does anything if we are usibg tags or are not on a mobile device, because the native select
+  //     elements on mobile devices tend to be better.  [Could maybe turn the element from a <select>
+  //     into a <datalist> if we are using tags?]
   // (b) wraps the select element in a <div> because in some places, eg in forms,  MRBS uses
   //     a table structure and because Select2 adds a sibling element the table structure is
   //     ruined.
@@ -47,7 +48,7 @@ jQuery.fn.extend({
   $select2_lang = basename(get_select2_lang_path(), '.js');
   ?>
   mrbsSelect: function(tags) {
-    if (!isMobile())
+    if (tags || !isMobile())
     {
       tags = Boolean(tags);
       $(this).wrap('<div></div>')
