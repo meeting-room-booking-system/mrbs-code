f74200bf27d3a39f70803a5f6c96d24df5679b4f
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-08-21 15:43:41 +0100

    Added form for adding a new role (doesn't do anything yet).

--- web/edit_role.php
+++ web/edit_role.php
@@ -1,7 +1,49 @@
 <?php
 namespace MRBS;
 
+use MRBS\Form\ElementFieldset;
+use MRBS\Form\ElementInputSubmit;
+use MRBS\Form\FieldDiv;
+use MRBS\Form\FieldInputText;
+use MRBS\Form\Form;
+
 require "defaultincludes.inc";
+
+
+function generate_add_form()
+{
+  $form = new Form();
+  $form->addHiddenInput('action', 'add')
+       ->setAttributes(array('action' => multisite(this_page()),
+                             'class'  => 'standard',
+                             'method' => 'post'));
+
+  // Name field
+  $fieldset = new ElementFieldset();
+  $field = new FieldInputText();
+  // Set a pattern as well as required to prevent a string of whitespace
+  $field->setLabel(get_vocab('role_name'))
+        ->setControlAttributes(array('name'     => 'name',
+                                     'required' => true,
+                                     'pattern'  => REGEX_TEXT_POS));
+  if (null !== ($maxlength = maxlength('roles.name')))
+  {
+    $field->setControlAttribute('maxlength', $maxlength);
+  }
+  $fieldset->addElement($field);
+  $form->addElement($fieldset);
+
+  // Submit button
+  $fieldset = new ElementFieldset();
+  $field = new FieldDiv();
+  $element = new ElementInputSubmit();
+  $element->setAttribute('value', get_vocab('add_role'));
+  $field->addControl($element);
+  $fieldset->addElement($field);
+  $form->addElement($fieldset);
+
+  $form->render();
+}
 
 
 // Check the user is authorised for this page
@@ -19,5 +61,7 @@ $context = array(
 
 print_header($context);
 
+echo "<h2>" . htmlspecialchars(get_vocab('roles')) . "</h2>";
+generate_add_form();
 
 print_footer();
