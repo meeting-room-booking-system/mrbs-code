01e00b763bb2926673df0a45c318b2376398c7cc
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-08-21 14:52:51 +0100

    Added missing file

---
+++ web/edit_role.php
@@ -0,0 +1,23 @@
+<?php
+namespace MRBS;
+
+require "defaultincludes.inc";
+
+
+// Check the user is authorised for this page
+checkAuthorised(this_page());
+
+$context = array(
+    'view'      => $view,
+    'view_all'  => $view_all,
+    'year'      => $year,
+    'month'     => $month,
+    'day'       => $day,
+    'area'      => isset($area) ? $area : null,
+    'room'      => isset($room) ? $room : null
+  );
+
+print_header($context);
+
+
+print_footer();
