d61ea884e3270b7e000ff9e39398d2c71e9a58e9
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-08-26 12:07:02 +0100

    Restructured

--- web/lib/MRBS/Areas.php
+++ web/lib/MRBS/Areas.php
@@ -2,68 +2,20 @@
 namespace MRBS;
 
 
-class Areas implements \Countable, \Iterator
+class Areas extends TableIterator
 {
-  private $res;
-  private $cursor;
-  private $item;
-
 
   public function __construct()
   {
-    $this->getAreas();
+    parent::__construct(__NAMESPACE__ . '\\Area');
   }
 
 
-  public function current()
+  protected function getRes()
   {
-    return $this->item;
-  }
-
-
-  public function next()
-  {
-    $this->cursor++;
-    if (false !== ($row = $this->res->next_row_keyed()))
-    {
-      $this->item = new Area();
-      $this->item->load($row);
-    }
-  }
-
-
-  public function key()
-  {
-    return $this->cursor;
-  }
-
-
-  public function valid()
-  {
-    return ($this->cursor < $this->count());
-  }
-
-
-  public function rewind()
-  {
-    if ($this->cursor >= 0)
-    {
-      $this->getAreas();
-    }
-    $this->next();
-  }
-
-
-  public function count()
-  {
-    return $this->res->count();
-  }
-
-
-  private function getAreas()
-  {
+    $class_name = $this->base_class;
     $sql = "SELECT *
-              FROM " . _tbl(Area::TABLE_NAME) . "
+              FROM " . _tbl($class_name::TABLE_NAME) . "
           ORDER BY sort_key";
     $this->res = db()->query($sql);
     $this->cursor = -1;
--- web/lib/MRBS/Roles.php
+++ web/lib/MRBS/Roles.php
@@ -2,69 +2,12 @@
 namespace MRBS;
 
 
-class Roles implements \Countable, \Iterator
+class Roles extends TableIterator
 {
-  private $res;
-  private $cursor;
-  private $item;
-
 
   public function __construct()
   {
-    $this->getRoles();
+    parent::__construct(__NAMESPACE__ . '\\Role');
   }
 
-
-  public function current()
-  {
-    return $this->item;
-  }
-
-
-  public function next()
-  {
-    $this->cursor++;
-    if (false !== ($row = $this->res->next_row_keyed()))
-    {
-      $this->item = new Role();
-      $this->item->load($row);
-    }
-  }
-
-
-  public function key()
-  {
-    return $this->cursor;
-  }
-
-
-  public function valid()
-  {
-    return ($this->cursor < $this->count());
-  }
-
-
-  public function rewind()
-  {
-    if ($this->cursor >= 0)
-    {
-      $this->getRoles();
-    }
-    $this->next();
-  }
-
-
-  public function count()
-  {
-    return $this->res->count();
-  }
-
-
-  private function getRoles()
-  {
-    $sql = "SELECT * FROM " . \MRBS\_tbl(Role::TABLE_NAME);
-    $this->res = \MRBS\db()->query($sql);
-    $this->cursor = -1;
-    $this->item = null;
-  }
 }
