f55035b6f695367359f769760692a6068f281988
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-08-31 09:09:46 +0100

    Added roles_rooms table

--- tables.my.sql
+++ tables.my.sql
@@ -267,7 +267,25 @@ CREATE TABLE mrbs_roles_areas
     ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE mrbs_roles_rooms
+(
+  role_id     int NOT NULL,
+  room_id     int NOT NULL,
+  permission  char CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  state       char CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+
+  UNIQUE KEY uq_role_room (role_id, room_id),
+  FOREIGN KEY (role_id)
+    REFERENCES mrbs_roles(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (room_id)
+    REFERENCES mrbs_room(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 INSERT INTO mrbs_variables (variable_name, variable_content)
-  VALUES ( 'db_version', '72');
+  VALUES ( 'db_version', '73');
 INSERT INTO mrbs_variables (variable_name, variable_content)
   VALUES ( 'local_db_version', '1');
