3d1e2703858e6092052ebeb662a56e999b6c7a07
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-10-06 15:11:54 +0100

    Created user_group table

--- web/upgrade/79/mysql.sql
+++ web/upgrade/79/mysql.sql
@@ -8,3 +8,20 @@ CREATE TABLE %DB_TBL_PREFIX%group
   PRIMARY KEY (id),
   UNIQUE KEY uq_group_name_auth_type (name, auth_type)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE %DB_TBL_PREFIX%user_group
+(
+  user_id     int NOT NULL,
+  group_id    int NOT NULL,
+
+  UNIQUE KEY uq_user_group (user_id, group_id),
+  FOREIGN KEY (user_id)
+    REFERENCES %DB_TBL_PREFIX%user(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (group_id)
+    REFERENCES %DB_TBL_PREFIX%group(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
