8b0245974cdd75271fcb4467ce0a83a9358b61fb
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-10-14 10:59:23 +0100

    Added basic table

--- web/edit_group.php
+++ web/edit_group.php
@@ -3,6 +3,38 @@ namespace MRBS;
 
 require "defaultincludes.inc";
 
+function generate_groups_table()
+{
+  global $auth;
+
+  $groups = new Groups();
+
+  echo "<table id=\"roles\">\n";
+  echo "<tbody>\n";
+
+  foreach ($groups as $group)
+  {
+    echo "<tr>";
+    if ($auth['type'] == 'db')
+    {
+      echo "<td>";
+      // TODO 1. Implement delete button
+      // generate_delete_button($group);
+      echo "</td>";
+    }
+
+    echo "<td>";
+    $href = multisite(this_page() . '?group_id=' . $group->id);
+    echo '<a href="' . htmlspecialchars($href). '">' . htmlspecialchars($group->name) . '</a>';
+    echo "</td>";
+    echo "</tr>\n";
+  }
+
+  echo "</tbody>\n";
+  echo "</table>\n";
+}
+
+
 // Check the user is authorised for this page
 checkAuthorised(this_page());
 
@@ -17,5 +49,9 @@ $context = array(
 );
 
 print_header($context);
+
+echo "<h2>" . htmlspecialchars(get_vocab('groups')) . "</h2>";
+
+generate_groups_table();
 
 print_footer();
