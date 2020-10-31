7e52e45644e2618ec4c1b864bebe0a63f258803e
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-10-31 11:06:46 +0000

    Fixed problem in recent commit

--- web/upgrade/78/mysql.sql
+++ web/upgrade/78/mysql.sql
@@ -1,5 +1,1 @@
--- Add columns for registration opens and closes
-
-ALTER TABLE %DB_TBL_PREFIX%entry 
-  ADD COLUMN registration_opens int DEFAULT NULL COMMENT 'Seconds before the start time',
-  ADD COLUMN registration_closes int DEFAULT NULL COMMENT 'Seconds before the start time';
+-- Null operation
--- web/upgrade/78/pgsql.sql
+++ web/upgrade/78/pgsql.sql
@@ -1,8 +1,1 @@
--- Add columns for registration opens and closes
 
-ALTER TABLE %DB_TBL_PREFIX%entry
-  ADD COLUMN registration_opens int DEFAULT NULL,
-  ADD COLUMN registration_closes int DEFAULT NULL;
-
-comment on column %DB_TBL_PREFIX%entry.registration_opens is 'Seconds before the start time';
-comment on column %DB_TBL_PREFIX%entry.registration_closes is 'Seconds before the start time';
