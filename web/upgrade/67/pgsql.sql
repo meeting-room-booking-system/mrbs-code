624ef1efa81db55d2f44345e5c99fe3f31720fc1
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-12-21 17:48:57 +0000

    Fixed problem in upgrade procedure

--- web/upgrade/67/pgsql.sql
+++ web/upgrade/67/pgsql.sql
@@ -2,7 +2,7 @@
 
 ALTER TABLE %DB_TBL_PREFIX%entry
   ADD COLUMN allow_registration       smallint DEFAULT 0 NOT NULL,
-  ADD COLUMN registrant_limit_enabled  smallint DEFAULT 1 NOT NULL,
+  ADD COLUMN enable_registrant_limit  smallint DEFAULT 1 NOT NULL,
   ADD COLUMN registrant_limit         int DEFAULT 0 NOT NULL;
 
 
