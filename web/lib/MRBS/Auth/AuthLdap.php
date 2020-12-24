25ef263048b0024bbb8cbc5cc127e417eee8153a
Campbell Morrison <cimorrison@users.sourceforge.net>
2020-12-24 15:26:25 +0000

    Added line number to debug information

--- web/lib/MRBS/Auth/AuthLdap.php
+++ web/lib/MRBS/Auth/AuthLdap.php
@@ -972,8 +972,12 @@ class AuthLdap extends Auth
 
     if ($ldap_debug)
     {
-      $caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1];
-      error_log("[DEBUG] " . $caller['class'] . $caller['type'] . $caller['function'] . ": $message");
+      list($called, $caller) = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+      error_log(
+          "[DEBUG] " .
+          $caller['class'] . $caller['type'] . $caller['function'] . '(' . $called['line'] . ')' .
+          ": $message"
+        );
     }
   }
 
