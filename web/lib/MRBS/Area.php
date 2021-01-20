c3bd34da9bde74496b933aaf64f888f6bab997e4
Campbell Morrison <cimorrison@users.sourceforge.net>
2021-01-20 16:57:25 +0000

    Restructured

--- web/lib/MRBS/Area.php
+++ web/lib/MRBS/Area.php
@@ -44,12 +44,54 @@ class Area extends Location
   }
 
 
+  // TODO: is this necessary?
   private static function sanitize(array $row)
   {
-    // Make sure the resolution is correct if we're using periods
+    global $periods, $private_override_options;
+    global $area_defaults;
+
+    // If there isn't a value in the database use the area default
+    foreach ($row as $key => $value)
+    {
+      if (!isset($row[$key]) && array_key_exists($key, $area_defaults))
+      {
+        $row[$key] = $area_defaults[$key];
+      }
+    }
+
+    // Make sure the slot settings are correct if we're using periods
     if (!empty($row['enable_periods']))
     {
       $row['resolution'] = 60;
+      $row['morningstarts'] = 12;
+      $row['morningstarts_minutes'] = 0;
+      $row['eveningends'] = 12;
+      if (array_key_exists('periods', $row))
+      {
+        $row['eveningends_minutes'] = count($row['periods']) - 1;
+      }
+      else
+      {
+        $row['eveningends_minutes'] = count($periods) - 1;
+      }
+    }
+    // Otherwise check that the resolution is set and > 0
+    elseif (array_key_exists('resolution', $row) &&
+            (empty($row['resolution']) || ($row['resolution'] < 0)))
+    {
+      $row['resolution'] = 30*60;  // 30 minutes, a reasonable fallback
+      $message = "Invalid value for 'resolution' in the area table.   Using ${row['resolution']} seconds.";
+      trigger_error($message, E_USER_WARNING);
+    }
+
+    // Do some sanity checking in case the area table is somehow messed up
+    // (1) 'private_override' must be a valid value
+    if (array_key_exists('private_override', $row) &&
+        !in_array($row['private_override'], $private_override_options))
+    {
+      $row['private_override'] = 'private';  // the safest default
+      $message = "Invalid value for 'private_override' in the area table.  Using '${row['private_override']}'.";
+      trigger_error($message, E_USER_WARNING);
     }
 
     return $row;
@@ -59,7 +101,7 @@ class Area extends Location
   // Function to decode any columns that are stored encoded in the database
   protected static function onRead(array $row)
   {
-    global $force_resolution, $area_defaults;
+    global $force_resolution, $area_defaults, $auth;
 
     // We cannot assume that any array keys exist as this may be called during an upgrade
     // process before the columns existed.
@@ -71,7 +113,7 @@ class Area extends Location
     }
 
     // TODO: Should this be necessary?  Shouldn't we just make sure the table
-    // TODO: contains the correct value in the first place?
+    // TODO: contains the correct values in the first place?
     $row = self::sanitize($row);
 
     // Some special config settings ...
@@ -83,6 +125,13 @@ class Area extends Location
         !$row['enable_periods'])
     {
       $row['resolution'] = $area_defaults['resolution'];
+    }
+
+    // If it's been set in the config settings then force all bookings to be shown
+    // as private for users who haven't logged in.
+    if ($auth['force_private_for_guests'] && (null === session()->getCurrentUser()))
+    {
+      $row['private_override'] = 'private';
     }
 
     return $row;
