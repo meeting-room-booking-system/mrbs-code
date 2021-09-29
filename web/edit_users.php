bcdb1e9b813c38c3c51f492b47cbbfcd6b87ecbe
campbell-m <87438215+campbell-m@users.noreply.github.com>
2021-09-29 13:38:09 +0100

    Added support for date custom fields in the user table.

--- web/edit_users.php
+++ web/edit_users.php
@@ -399,6 +399,10 @@ function get_field_custom($custom_field, $params, $disabled=false)
   {
     $class = 'FieldTextarea';
   }
+  elseif ($custom_field['type'] == 'date')
+  {
+    $class = 'FieldInputDate';
+  }
   elseif (!empty($select_options[$params['field']]))
   {
     $class = 'FieldSelect';
@@ -436,6 +440,10 @@ function get_field_custom($custom_field, $params, $disabled=false)
     case 'FieldSelect':
       $options = $select_options[$params['field']];
       $field->addSelectOptions($options, $params['value']);
+      break;
+
+    case 'FieldInputDate':
+      $field->setControlAttribute('value', $params['value']);
       break;
 
     case 'FieldInputDatalist':
