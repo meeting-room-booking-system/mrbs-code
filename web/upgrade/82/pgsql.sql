3eac4657c23e117077511616f45924bf835daae4
campbell-m <87438215+campbell-m@users.noreply.github.com>
2021-11-22 17:21:40 +0000

    Fixed merge

--- web/upgrade/82/pgsql.sql
+++ web/upgrade/82/pgsql.sql
@@ -1,12 +1,1 @@
--- Various fixes to bring PostgreSQL in line with MySQL
 
-ALTER TABLE %DB_TBL_PREFIX%entry
-  ALTER COLUMN create_by SET DEFAULT '',
-  ALTER COLUMN modified_by SET DEFAULT '',
-  ALTER COLUMN name SET DEFAULT '';
-
-ALTER TABLE %DB_TBL_PREFIX%repeat
-  ALTER COLUMN rep_opt SET DEFAULT '',
-  ALTER COLUMN create_by SET DEFAULT '',
-  ALTER COLUMN modified_by SET DEFAULT '',
-  ALTER COLUMN name SET DEFAULT '';
