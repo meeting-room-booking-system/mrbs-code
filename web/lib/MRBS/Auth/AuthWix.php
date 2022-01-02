7026b3c6b7232d581de47411dddb3aa5216516dd
campbell-m <87438215+campbell-m@users.noreply.github.com>
2022-01-02 14:36:32 +0000

    Fixed again

--- web/lib/MRBS/Auth/AuthWix.php
+++ web/lib/MRBS/Auth/AuthWix.php
@@ -58,7 +58,7 @@ class AuthWix extends Auth
     $result = $this->http_functions('getMemberByEmail', $params);
 
     // The username doesn't exist - return NULL
-    if (!isset($result) || ($result === false))
+    if (($result === false) || ($result = json_encode(null)))
     {
       return null;
     }
