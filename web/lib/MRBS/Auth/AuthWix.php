456d47cb2b44ad737107e07dd312da00ed9653b9
campbell-m <87438215+campbell-m@users.noreply.github.com>
2022-01-27 17:59:44 +0000

    Wix authentication: switched to using badges instead of custom fields to determine MRBS admins as custom fields can be edited by members themselves in their profile.

--- web/lib/MRBS/Auth/AuthWix.php
+++ web/lib/MRBS/Auth/AuthWix.php
@@ -101,7 +101,7 @@ class AuthWix extends Auth
     $user->level = $this->getDefaultLevel($username);
 
     // Then if they are not an admin get their admin status from Wix
-    if (($user->level < 2) && !empty($result->contact->info->extendedFields->{$auth['wix']['admin_property']}))
+    if (($user->level < 2) && in_array($auth['wix']['admin_badge'], $result->badges))
     {
       $user->level = 2;
     }
