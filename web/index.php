6f73ea66ea55ab59cdfa2cb2d0a2cc0d6065e541
campbell-m <87438215+campbell-m@users.noreply.github.com>
2022-01-29 17:45:55 +0000

    Simplified code

--- web/index.php
+++ web/index.php
@@ -239,7 +239,7 @@ function get_view_nav($current_view, $view_all, $year, $month, $day, $area, $roo
 
   foreach ($views as $view => $token)
   {
-    $this_view_all = (isset($view_all)) ? $view_all : 1;
+    $this_view_all = $view_all ?? 1;
 
     $vars = array('view'      => $view,
                   'view_all'  => $this_view_all,
@@ -455,7 +455,7 @@ $context = array(
     'month'     => $month,
     'day'       => $day,
     'area'      => $area,
-    'room'      => isset($room) ? $room : null
+    'room'      => $room ?? null
   );
 
 print_header($context);
