c4db8d0153f40e5031b10e483e3edf113aa83914
campbell-m <87438215+campbell-m@users.noreply.github.com>
2022-09-03 18:13:10 +0100

    Added SensitiveParameter attribute for database credentials

--- web/lib/MRBS/DBFactory.php
+++ web/lib/MRBS/DBFactory.php
@@ -6,8 +6,19 @@ namespace MRBS;
 // A helper class to build a DB object, dependent on the database type required
 class DBFactory
 {
-  //
-  public static function create($db_system, $db_host, $db_username, $db_password, $db_name, $persist=false, $db_port=null)
+  // The SensitiveParameter attribute needs to be on a separate line for PHP 7.
+  // The attribute is only recognised by PHP 8.2 and later.
+  public static function create(
+    $db_system,
+    $db_host,
+    #[SensitiveParameter]
+    $db_username,
+    #[SensitiveParameter]
+    $db_password,
+    #[SensitiveParameter]
+    $db_name,
+    $persist=false,
+    $db_port=null)
   {
     switch ($db_system)
     {
