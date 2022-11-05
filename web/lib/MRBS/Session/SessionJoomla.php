a71c805fa2da0e3d397a203a90d122bc3e4c53bf
campbell-m <87438215+campbell-m@users.noreply.github.com>
2022-11-05 18:13:40 +0000

    Replaced qualifier with import

--- web/lib/MRBS/Session/SessionJoomla.php
+++ web/lib/MRBS/Session/SessionJoomla.php
@@ -3,6 +3,7 @@ namespace MRBS\Session;
 
 use MRBS\JFactory;
 use MRBS\User;
+use function MRBS\auth;
 
 require_once MRBS_ROOT . '/auth/cms/joomla.inc';
 
@@ -60,7 +61,7 @@ class SessionJoomla extends SessionWithLogin
 
   public function getCurrentUser() : ?User
   {
-    return \MRBS\auth()->getCurrentUser();
+    return auth()->getCurrentUser();
   }
 
 
