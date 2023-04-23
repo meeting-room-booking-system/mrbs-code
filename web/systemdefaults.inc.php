c9e68b0b4d86f024ad1740f5c6c416b930461b89
campbell-m <87438215+campbell-m@users.noreply.github.com>
2023-04-23 18:09:24 +0100

    Converts repeat end date in view_entry

--- web/systemdefaults.inc.php
+++ web/systemdefaults.inc.php
@@ -436,6 +436,13 @@ $strftime_format['view_week_start']    = "%e ";        // year and month the sam
 $strftime_format['view_week_start_m']  = "%e %B ";     // just the year the same
 $strftime_format['view_week_start_y']  = "%e %B %Y ";  // years (and months) different
 
+
+// The format used for dates
+$datetime_formats['date'] = array(
+  'date_type' => IntlDateFormatter::FULL,
+  'time_type' => IntlDateFormatter::NONE
+);
+
 // The format used for dates with times
 $datetime_formats['date_and_time'] = array(
   'date_type' => IntlDateFormatter::FULL,
