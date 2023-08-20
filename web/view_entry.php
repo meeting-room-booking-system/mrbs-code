42409e4c6cf073e3fa2b26601e64f11bdc09dada
campbell-m <87438215+campbell-m@users.noreply.github.com>
2023-08-20 17:50:39 +0100

    Format code

--- web/view_entry.php
+++ web/view_entry.php
@@ -983,13 +983,14 @@ if (!$auth['only_admin_can_copy_others_entries'] || $writeable)
   }
   if ((!empty($repeat_id) || $series) && $repeats_allowed) {
     echo "<div>\n";
-    $params = array('action' => multisite("edit_entry.php?day=$day&month=$month&year=$year"),
-      'value' => get_vocab('copyseries'),
-      'inputs' => array('id' => $id,
-      'edit_series' => true,
-      'copy' => 1,
-      'returl' => $returl)
-    );
+    $params = array(
+        'action'      => multisite("edit_entry.php?day=$day&month=$month&year=$year"),
+        'value'       => get_vocab('copyseries'),
+        'inputs'      => array('id' => $id,
+        'edit_series' => true,
+        'copy'        => 1,
+        'returl'      => $returl)
+      );
     generate_button($params);
     echo "</div>\n";
   }
