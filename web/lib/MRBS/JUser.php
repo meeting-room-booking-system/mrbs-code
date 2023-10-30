6937eb16fd0de3f903382571abc1eae8f8e3f03d
campbell-m <87438215+campbell-m@users.noreply.github.com>
2023-10-30 17:50:39 +0000

    Fix authentication against Joomla Version 5. See GitHub Issue #3547.

--- web/lib/MRBS/JUser.php
+++ web/lib/MRBS/JUser.php
@@ -3,10 +3,11 @@
 namespace MRBS;
 
 class JUser extends \JUser {
- 
+
   // NOTE:  In some versions of Joomla!, JUser::getAuthorisedGroups() seems to reset the
   // timezone to the user's Joomla timezone, which may be different from the MRBS timezone, so
   // we have to get the timezone before calling it and restore it afterwards.
+  // (This doesn't seem to happen in Joomla 5. It may also be OK in some earlier versions.)
   public function getAuthorisedGroups()
   {
     $tz = date_default_timezone_get();
@@ -14,7 +15,7 @@ class JUser extends \JUser {
     date_default_timezone_set($tz);
     return $result;
   }
-  
+
   // Not sure whether getAuthorisedViewLevels() has the same problem, but just in case ...
   public function getAuthorisedViewLevels()
   {
