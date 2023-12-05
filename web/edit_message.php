97441f18a4e6d12fca7b74af61c227254cdfb626
campbell-m <87438215+campbell-m@users.noreply.github.com>
2023-12-05 14:56:35 +0000

    Add submit buttons

--- web/edit_message.php
+++ web/edit_message.php
@@ -3,10 +3,44 @@ declare(strict_types=1);
 namespace MRBS;
 
 use MRBS\Form\ElementFieldset;
+use MRBS\Form\ElementInputSubmit;
+use MRBS\Form\FieldDiv;
 use MRBS\Form\FieldTextarea;
 use MRBS\Form\Form;
 
 require 'defaultincludes.inc';
+
+function get_fieldset_submit_buttons() : ElementFieldset
+{
+  $fieldset = new ElementFieldset();
+
+  // The back and submit buttons
+  $field = new FieldDiv();
+
+  $back = new ElementInputSubmit();
+  $back->setAttributes(array(
+    'name'           => 'back_button',
+    'value'          => get_vocab('back'),
+    'formnovalidate' => true)
+  );
+
+  $submit = new ElementInputSubmit();
+  $submit->setAttributes(array(
+    'class' => 'default_action',
+    'name'  => 'save_button',
+    'value' => get_vocab('save'))
+  );
+
+  $field->setAttribute('class', 'submit_buttons')
+        ->addLabelClass('no_suffix')
+        ->addLabelElement($back)
+        ->addControlElement($submit);
+
+  $fieldset->addElement($field);
+
+  return $fieldset;
+}
+
 
 // Check the user is authorised for this page
 checkAuthorised(this_page());
@@ -42,6 +76,8 @@ $message_field->setLabel(get_vocab('message'))
 $fieldset->addElement($message_field);
 
 $form->addElement($fieldset);
+
+$form->addElement(get_fieldset_submit_buttons());
 
 $form->render();
 
