8675c8fa6ade5db0d8a0292d91415f14d66ca2f3
campbell-m <87438215+campbell-m@users.noreply.github.com>
2024-07-20 15:58:54 +0100

    Take content type from server

--- web/js/search.js.php
+++ web/js/search.js.php
@@ -60,41 +60,47 @@ $(document).on('page_ready', function() {
             url: window.location.href,
             data: data,
             success: function(blob, status, xhr) {
-            // check for a filename
-            var filename = "";
-            var disposition = xhr.getResponseHeader('Content-Disposition');
-            if (disposition && disposition.indexOf('attachment') !== -1) {
-              var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-              var matches = filenameRegex.exec(disposition);
-              if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
-            }
-
-            if (typeof window.navigator.msSaveBlob !== 'undefined') {
-              // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-              window.navigator.msSaveBlob(blob, filename);
-            } else {
-              var URL = window.URL || window.webkitURL;
-              var downloadUrl = URL.createObjectURL(new Blob([blob], {type: 'application/ics'}));
-
-              if (filename) {
-                // use HTML5 a[download] attribute to specify filename
-                var a = document.createElement("a");
-                // safari doesn't support this yet
-                if (typeof a.download === 'undefined') {
-                  window.location.href = downloadUrl;
-                } else {
-                  a.href = downloadUrl;
-                  a.download = filename;
-                  document.body.appendChild(a);
-                  a.click();
-                }
-              } else {
-                window.location.href = downloadUrl;
+              <?php
+              // See https://stackoverflow.com/questions/16086162/handle-file-download-from-ajax-post
+              ?>
+              var contentType = xhr.getResponseHeader('Content-Type');
+              var contentTypeRegex = /^[^;]*/;
+              var matches = contentType.match(contentTypeRegex);
+              var type = (matches === null) ? '' : matches[0];
+              var filename = "";
+              var disposition = xhr.getResponseHeader('Content-Disposition');
+              if (disposition && disposition.indexOf('attachment') !== -1) {
+                var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                matches = filenameRegex.exec(disposition);
+                if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
               }
 
-              setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+              if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                window.navigator.msSaveBlob(blob, filename);
+              } else {
+                var URL = window.URL || window.webkitURL;
+                var downloadUrl = URL.createObjectURL(new Blob([blob], {type: type}));
+
+                if (filename) {
+                  // use HTML5 a[download] attribute to specify filename
+                  var a = document.createElement("a");
+                  // safari doesn't support this yet
+                  if (typeof a.download === 'undefined') {
+                    window.location.href = downloadUrl;
+                  } else {
+                    a.href = downloadUrl;
+                    a.download = filename;
+                    document.body.appendChild(a);
+                    a.click();
+                  }
+                } else {
+                  window.location.href = downloadUrl;
+                }
+
+                setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+              }
             }
-          }
           });
         }
       }
