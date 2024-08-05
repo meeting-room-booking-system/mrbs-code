d472e5ebe943c3e24e9ade02589a34e59be34c91
campbell-m <87438215+campbell-m@users.noreply.github.com>
2024-08-05 15:40:35 +0100

    Fix potential bug

--- web/lib/MRBS/Auth/AuthLdap.php
+++ web/lib/MRBS/Auth/AuthLdap.php
@@ -556,7 +556,7 @@ class AuthLdap extends Auth
     $res = ldap_search($ldap, $base_dn, $filter, $attributes);
     $t = self::getProfileClock();
 
-    if ($res == false)
+    if ($res === false)
     {
       self::debug("ldap_search failed: " . self::ldapError($ldap));
       return false;
