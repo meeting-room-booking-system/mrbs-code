93fd6e60c8e945135f0851b74fe172aaef40a257
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-01-13 17:50:28 +0000

    Fix bug causing incorrect end time to be shown in reports when using periods.

--- web/report.php
+++ web/report.php
@@ -960,7 +960,9 @@ function report_row(&$rows, $data)
       case 'start_time':
         if ($data['enable_periods'])
         {
-          list( , $date) =  period_date_string($value, $data['area_id']);
+          // If we've fallen through from 'end_time' we need to set previous_period to TRUE
+          // so that we get the name of the previous period, not this one.
+          list( , $date) =  period_date_string($value, $data['area_id'], ($field == 'end_time'));
         }
         else
         {
