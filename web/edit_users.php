0229c13304f6e4c00dbbce3797e8d57f940ba565
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-02-24 17:39:41 +0000

    Restructure

--- web/edit_users.php
+++ web/edit_users.php
@@ -116,38 +116,6 @@ function can_edit_user($target)
 }
 
 
-// Get the type that should be used with get_form_var() for
-// a field which is a member of the array returned by get_field_info()
-function get_form_var_type(array $field) : string
-{
-  // "Level" is an exception because we've forced the value to be a string
-  // so that it can be used in an associative array
-  if ($field['name'] == 'level')
-  {
-    return 'string';
-  }
-
-  switch($field['nature'])
-  {
-    case 'character':
-      $type = 'string';
-      break;
-    case 'integer':
-      // Smallints and tinyints are considered to be booleans
-      $type = (isset($field['length']) && ($field['length'] <= 2)) ? 'string' : 'int';
-      break;
-    case 'real':
-      $type = 'float';
-      break;
-    // We can only really deal with the types above at the moment
-    default:
-      $type = 'string';
-      break;
-  }
-  return $type;
-}
-
-
 function output_row($row)
 {
   global $is_ajax, $json_data;
