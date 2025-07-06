ae7a6a97865df3c6a1263a0e18e25ba2bbf9b01f
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-07-06 14:11:26 +0100

    Rename variables

---
+++ web/lib/MRBS/Calendar/CalendarMonthOneRoom.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace MRBS\Calendar;
+
+class CalendarMonthOneRoom extends Calendar
+{
+
+  public function __construct(string $view, int $view_all, int $year, int $month, int $day, int $area, int $room)
+  {
+  }
+
+
+  public function innerHTML(): string
+  {
+    // TODO: Implement innerHTML() method.
+  }
+}
