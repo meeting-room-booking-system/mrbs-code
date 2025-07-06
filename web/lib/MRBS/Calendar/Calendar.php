4e65773ea70f2ac3c2ebf688a9db954c5104e35c
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-07-06 14:31:02 +0100

    Restructure

--- web/lib/MRBS/Calendar/Calendar.php
+++ web/lib/MRBS/Calendar/Calendar.php
@@ -4,5 +4,14 @@ namespace MRBS\Calendar;
 
 abstract class Calendar
 {
+  protected $day;
+  protected $month;
+  protected $year;
+  protected $area_id;
+  protected $room_id;
+  protected $view;
+  protected $view_all;
+
+
   abstract public function innerHTML() : string;
 }
