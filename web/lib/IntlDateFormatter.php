de78ed332faaae78e39dfe4c8ce939873d227f8d
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-12-01 17:07:59 +0000

    Add DocBlock.

--- web/lib/IntlDateFormatter.php
+++ web/lib/IntlDateFormatter.php
@@ -1,10 +1,12 @@
 <?php
 declare(strict_types=1);
-// An emulation of the IntlDateFormatter class for use when the intl extension
-// is not loaded.  When it is loaded we still have the option of using the
-// emulation which sometimes gives better results if the ICU library is out of
-// date.
 
+/**
+ * An emulation of the IntlDateFormatter class for use when the intl extension is not loaded.
+ *
+ * When it is loaded we still have the option of using the emulation which (a) is useful for
+ * testing and (b) sometimes gives better results if the ICU library is out of date.
+ */
 class IntlDateFormatter extends \MRBS\Intl\IntlDateFormatter
 {
 
