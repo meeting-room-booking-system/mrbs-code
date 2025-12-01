1ed3883e19b01f5b093501c0e999fa9cfc0899a8
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-12-01 18:30:11 +0000

    Start an emulation of the Collator class.

---
+++ web/lib/MRBS/Intl/Collator.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+namespace MRBS\Intl;
+
+
+/**
+ * A partial emulation of the PHP \Collator class.
+ */
+class Collator
+{
+  public const DEFAULT_VALUE = -1;
+  public const PRIMARY = 0;
+  public const SECONDARY = 1;
+  public const TERTIARY = 2;
+  public const DEFAULT_STRENGTH = 2;
+  public const QUATERNARY = 3;
+  public const IDENTICAL = 15;
+  public const OFF = 16;
+  public const ON = 17;
+  public const SHIFTED = 20;
+  public const NON_IGNORABLE = 21;
+  public const LOWER_FIRST = 24;
+  public const UPPER_FIRST = 25;
+  public const FRENCH_COLLATION = 0;
+  public const ALTERNATE_HANDLING = 1;
+  public const CASE_FIRST = 2;
+  public const CASE_LEVEL = 3;
+  public const NORMALIZATION_MODE = 4;
+  public const STRENGTH = 5;
+  public const HIRAGANA_QUATERNARY_MODE =6;
+  public const NUMERIC_COLLATION = 7;
+  public const SORT_REGULAR = 0;
+  public const SORT_STRING = 1;
+  public const SORT_NUMERIC = 2;
+
+  public function __construct(string $locale)
+  {
+
+  }
+}
