847b9f29e601385b5bb120b33ddc95ff1ab3a94a
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-12-02 18:05:32 +0000

    Add remaining stubs

--- web/lib/MRBS/Intl/Collator.php
+++ web/lib/MRBS/Intl/Collator.php
@@ -27,7 +27,7 @@ class Collator
   public const CASE_LEVEL = 3;
   public const NORMALIZATION_MODE = 4;
   public const STRENGTH = 5;
-  public const HIRAGANA_QUATERNARY_MODE =6;
+  public const HIRAGANA_QUATERNARY_MODE = 6;
   public const NUMERIC_COLLATION = 7;
   public const SORT_REGULAR = 0;
   public const SORT_STRING = 1;
@@ -36,6 +36,9 @@ class Collator
   private $locale;
 
 
+  /**
+   * @see \Collator::__construct()
+   */
   public function __construct(string $locale)
   {
     $this->locale = $locale;
@@ -43,10 +46,126 @@ class Collator
 
 
   /**
-   * @see \Collator::asort
+   * @see \Collator::asort()
    */
   public function asort(array &$array, int $flags = self::SORT_REGULAR): bool
   {
     throw new \Exception("Not yet implemented");
   }
+
+
+  /**
+   * @return int<-1,1>|false
+   * @see \Collator::compare()
+   */
+  public function compare(string $string1, string $string2)
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @see \Collator::create()
+   */
+  public static function create(string $locale): ?self
+  {
+    return new self($locale);
+  }
+
+
+  /**
+   * @return int|false
+   * @see \Collator::getAttribute()
+   */
+  public function getAttribute(int $attribute)
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @return int|false
+   * @see \Collator::getErrorCode()
+   */
+  public function getErrorCode()
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @return string|false
+   * @see \Collator::getErrorMessage()
+   */
+  public function getErrorMessage()
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @return string|false
+   * @see \Collator::getLocale()
+   */
+  public function getLocale()
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @return string|false
+   * @see \Collator::getSortKey()
+   */
+  public function getSortKey(string $string)
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @see \Collator::getStrength()
+   */
+  public function getStrength(): int
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @see \Collator::setAttribute()
+   */
+  public function setAttribute(int $attribute, int $value): bool
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @see \Collator::setStrength()
+   * @return true
+   */
+  public function setStrength(int $strength)
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @see \Collator::sort()
+   */
+  public function sort(array &$array, int $flags = self::SORT_REGULAR): bool
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
+
+  /**
+   * @see \Collator::sortWithSortKeys()
+   */
+  public function sortWithSortKeys(array &$array): bool
+  {
+    throw new \Exception("Not yet implemented");
+  }
+
 }
