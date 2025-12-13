3d6c700a089d94bfb6bc5ad1d1d59dc17c217f69
campbell-m <87438215+campbell-m@users.noreply.github.com>
2025-12-13 17:35:05 +0000

    Start NumberFormatter emulation

---
+++ test/test_number_formatter.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+namespace MRBS;
+
+
+include 'defaultincludes.inc';
+require_once 'functions_test.inc';
+
+error_reporting(-1);
+ini_set('display_errors', '1');
+
+$color_fail = 'pink';
+$color_pass = 'palegreen';
+
+function test_constants($class)
+{
+  echo "<h1>Testing constants</h1>\n";
+  $passed = true;
+  $php_constants = (new \ReflectionClass($class))->getConstants();
+  $emulation_constants = (new \ReflectionClass("MRBS\Intl\\$class"))->getConstants();
+  foreach ($php_constants as $name => $value)
+  {
+    if (!isset($emulation_constants[$name]))
+    {
+      $passed = false;
+      echo "Failed to find constant $name<br>\n";
+    }
+    else if ($value != $emulation_constants[$name])
+    {
+      $passed = false;
+      echo "Constant $name has different value in PHP ($value) and MRBS ($emulation_constants[$name])<br>\n";
+    }
+  }
+  if ($passed)
+  {
+    echo "Passed<br>\n";
+  }
+}
+
+$loaded_extensions = get_loaded_extensions();
+
+echo "PHP version: " . PHP_VERSION;
+echo "<br>\n";
+echo "mbstring enabled: " . var_export(in_array('mbstring', $loaded_extensions), true);
+echo "<br>\n";
+echo "intl enabled: " . var_export(in_array('intl', $loaded_extensions), true);
+echo "<br>\n";
+echo "<br>\n";
+
+if (!in_array('intl', $loaded_extensions))
+{
+  die("This test needs the 'intl' PHP extension to be loaded.");
+}
+
+test_constants('NumberFormatter');
