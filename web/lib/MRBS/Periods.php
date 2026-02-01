bde479eb2f2c96371eeafc08681055936ba9353f
campbell-m <87438215+campbell-m@users.noreply.github.com>
2026-02-01 15:09:42 +0000

    Remove static storing of data.

--- web/lib/MRBS/Periods.php
+++ web/lib/MRBS/Periods.php
@@ -6,78 +6,67 @@ class Periods implements \Countable, \Iterator
 {
   private $area_id;
   private $index = 0;
-  private static $data;
+  private $data = [];
 
 
   private function __construct(int $area_id)
   {
     $this->area_id = $area_id;
-    if (!isset(self::$data[$area_id]))
-    {
-      self::$data[$area_id] = [];
-    }
   }
 
 
   /**
-   * Get the periods for a given area.
-   *
-   * @return false|self
+   * Get the periods for a given area from the database.
    */
-  public static function getForArea(int $area_id)
+  public static function getForArea(int $area_id) : self
   {
-    if (!isset(self::$data))
+    $result = new self($area_id);
+
+    $sql = "SELECT id, periods
+              FROM " . _tbl('area') . "
+             WHERE id=:id";
+    $res = db()->query($sql, [':id' => $area_id]);
+
+    while (false !== ($row = $res->next_row_keyed()))
     {
-      self::$data = [];
-      $sql = "SELECT id, periods
-                FROM " . _tbl('area');
-      $res = db()->query($sql);
+      $periods = json_decode($row['periods'], true);
 
-      while (false !== ($row = $res->next_row_keyed()))
+      // The periods are stored in the database as either:
+      //   (a) a simple array of period names (the old way of storing periods); or
+      //   (b) an associative array of period names and start/end times.
+      if (is_assoc($periods))
       {
-        $this_area_periods = [];
-        $periods = json_decode($row['periods'], true);
-
-        // The periods are stored in the database as either:
-        //   (a) a simple array of period names (the old way of storing periods); or
-        //   (b) an associative array of period names and start/end times.
-        if (is_assoc($periods))
+        foreach ($periods as $period_name => $times)
         {
-          foreach ($periods as $period_name => $times)
-          {
-            $this_area_periods[] = new Period(
-              $period_name,
-              $times['start'],
-              $times['end']
-            );
-          }
+          $result->add(new Period(
+            $period_name,
+            $times['start'],
+            $times['end']
+          ));
         }
-        else
+      }
+      else
+      {
+        foreach ($periods as $period_name)
         {
-          foreach ($periods as $period_name)
-          {
-            $this_area_periods[] = new Period(
-              $period_name
-            );
-          }
+          $result->add(new Period(
+            $period_name
+          ));
         }
-
-        self::$data[$row['id']] = $this_area_periods;
       }
     }
 
-    if (!isset(self::$data[$area_id]))
-    {
-      return false;
-    }
-
-    return new self($area_id);
+    return $result;
   }
 
+  private function add(Period $period) : void
+  {
+    $this->data[] = $period;
+  }
 
   public function current() : Period
   {
-    return self::$data[$this->area_id][$this->index];
+    return $this->data[$this->index];
   }
 
   public function next() : void
@@ -92,7 +81,7 @@ class Periods implements \Countable, \Iterator
 
   public function valid(): bool
   {
-    return isset(self::$data[$this->area_id][$this->index]);
+    return isset($this->data[$this->index]);
   }
 
   public function rewind() : void
@@ -102,7 +91,7 @@ class Periods implements \Countable, \Iterator
 
   public function count() : int
   {
-    return count(self::$data[$this->area_id]);
+    return count($this->data);
   }
 
 }
