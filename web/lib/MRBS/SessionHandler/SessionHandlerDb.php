a581d99ca895644ef1df069ecbceb8dd9bb17438
campbell-m <87438215+campbell-m@users.noreply.github.com>
2026-09-04 18:53:53 +0100

    Add comment on return value of read() method.

--- web/lib/MRBS/SessionHandler/SessionHandlerDb.php
+++ web/lib/MRBS/SessionHandler/SessionHandlerDb.php
@@ -141,6 +141,9 @@ class SessionHandlerDb extends SessionHandlerAbstract
   }
 
 
+  // TODO: Note that this method actually returns an empty string if nothing was read, rather than false.
+  // TODO: More work is needed to determine when sn empty string should be returned and when FALSE.
+  // TODO: See https://stackoverflow.com/questions/68025570/return-value-of-sessionhandlerinterface-methods-in-php
   public function read($id)
   {
     global $dbsys;
