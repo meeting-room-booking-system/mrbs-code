06f274dad77031ef41871d7631479e04b9475af4
campbell-m <87438215+campbell-m@users.noreply.github.com>
2026-09-13 17:42:25 +0100

    Replace var with let or const.

--- web/js/admin.js.php
+++ web/js/admin.js.php
@@ -5,22 +5,22 @@ namespace MRBS;
 require "../defaultincludes.inc";
 
 http_headers(array("Content-type: application/x-javascript"),
-             60*30);  // 30 minute expiry
+             60*30);  // 30-minute expiry
 ?>
 
 'use strict';
 
 $(document).on('page_ready', function() {
 
-  var tableOptions = {};
-  var fixedColumnsOptions = {leftColumns: 1};
+  const tableOptions = {};
+  const fixedColumnsOptions = {leftColumns: 1};
 
   <?php // Get the types and feed those into dataTables ?>
   tableOptions.columnDefs = getTypes($('#rooms_table'));
 
   <?php
   // Turn the list of rooms into a dataTable
-  // If we're an admin, then fix the right hand column
+  // If we're an admin, then fix the right-hand column
   ?>
   if (args.isAdmin)
   {
