c1cdf5bdd05152ef98e0e9dc610551e2706b15bf
campbell-m <87438215+campbell-m@users.noreply.github.com>
2026-09-17 16:38:10 +0100

    Delete unused function.

--- web/js/functions.js.php
+++ web/js/functions.js.php
@@ -258,31 +258,6 @@ function getCSRFToken()
 }
 
 
-<?php
-// Get a query string parameter from a url
-// See https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-?>
-function getParameterByName(name, url)
-{
-  if (!url)
-  {
-    url = window.location.href;
-  }
-  name = name.replace(/[\[\]]/g, "\\$&");
-  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-  const results = regex.exec(url);
-  if (!results)
-  {
-    return null;
-  }
-  if (!results[2])
-  {
-    return '';
-  }
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-
 (function($){
 
     /**
