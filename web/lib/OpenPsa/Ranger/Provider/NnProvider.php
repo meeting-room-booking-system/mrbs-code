e9989687e883b2f0fd2e054d12e27a4cc913c70d
campbell-m <87438215+campbell-m@users.noreply.github.com>
2026-09-22 11:44:54 +0100

    Update Ranger to V0.6.1, fixing GitHub Issue #4055.

---
+++ web/lib/OpenPsa/Ranger/Provider/NnProvider.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @copyright CONTENT CONTROL GmbH, http://www.contentcontrol-berlin.de
+ * @author CONTENT CONTROL GmbH, http://www.contentcontrol-berlin.de
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+namespace OpenPsa\Ranger\Provider;
+
+class NnProvider extends NoProvider
+{
+}
